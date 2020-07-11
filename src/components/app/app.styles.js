0a598df146f20d52b7f64ff492c315e06a2fdbd2
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-07-11 23:28:27 +0300

    changed capitalization

--- src/components/app/app.styles.js
+++ src/components/app/app.styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(() => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center'
+    display: 'flex'
   }
 }));
