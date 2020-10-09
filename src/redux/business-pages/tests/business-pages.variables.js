ea427b3df0582d70eb72c723c577a333704cb18b
Dmytro Didukh <jeriko.outback@gmail.com>
2020-10-09 14:53:31 +0300

    variables for tests

---
+++ src/redux/business-pages/tests/business-pages.variables.js
@@ -0,0 +1,88 @@
+export const ukText = 'Український текст бізнес сторінки';
+export const enText = 'English text of business page';
+export const businessPage = {
+  _id: 'de57efa1gdg543414b430',
+  code: 'business-page',
+  title: [{ value: 'Укр заголовок' }, { value: 'Eng title' }],
+  text: [
+    {
+      value: ukText
+    },
+    {
+      value: enText
+    }
+  ]
+};
+export const businessPageId = '0c3c7954dd35de268bed4fe8';
+export const businessPageToRemoveId = '0c3c7954dd35de268bed4fe8';
+export const businessPages = [
+  {
+    _id: 'de57efa1gdg543414b430',
+    code: 'about-us-test',
+    title: [{ value: 'Про нас' }, { value: 'About us' }],
+    text: [
+      {
+        value: `${ukText} про нас`
+      },
+      {
+        value: `${enText} about us`
+      }
+    ]
+  },
+  {
+    _id: 'fdc7529135f2c050c877a67a',
+    code: 'privacy-policy-test',
+    title: [{ value: 'Умови конфіденційності' }, { value: 'Privacy policy' }],
+    text: [
+      {
+        value: `${ukText} умови конфіденційності`
+      },
+      {
+        value: `${ukText} privacy policy`
+      }
+    ]
+  }
+];
+
+export const businessPageToUpdate = {
+  id: 'de57efa1gdg543414b430',
+  page: {
+    title: [
+      {
+        lang: 'uk',
+        value: `${ukText} про нас оновлена`
+      },
+      {
+        lang: 'en',
+        value: `${enText} about us updated`
+      }
+    ]
+  }
+};
+export const fakeBusinessPage = {
+  data: {
+    getBusinessTextById: {
+      _id: '0c3c7929dd85de268bed4fe8',
+      title: [
+        {
+          lang: 'uk',
+          value: 'Пустишка'
+        },
+        {
+          lang: 'en',
+          value: 'Fake'
+        }
+      ],
+      text: [
+        {
+          lang: 'uk',
+          value: 'Текс пустишка'
+        },
+        {
+          lang: 'en',
+          value: 'Fake text'
+        }
+      ]
+    }
+  }
+};
