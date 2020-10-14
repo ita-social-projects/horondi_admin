8c5fe7861a191ffbc6b5a33df82d7ff64d7cc9a9
Dmytro Didukh <jeriko.outback@gmail.com>
2020-10-14 15:54:15 +0300

    Q/A action and types #123

---
+++ src/redux/email-questions/email-questions.actions.js
@@ -0,0 +1,68 @@
+import {
+  GET_ALL_EMAIL_QUESTIONS,
+  SET_ALL_EMAIL_QUESTIONS,
+  GET_EMAIL_QUESTION_BY_ID,
+  SET_CURRENT_EMAIL_QUESTION,
+  MAKE_QUESTION_SPAM,
+  ANSWER_TO_EMAIL_QUESTION,
+  DELETE_EMAIL_QUESTION,
+  SET_EMAIL_QUESTIONS_LOADING,
+  SET_EMAIL_QUESTIONS_ERROR
+} from './email-questions.types';
+
+const setAllEmailQuestion = (questionsList) => ({
+  type: SET_ALL_EMAIL_QUESTIONS,
+  payload: questionsList
+});
+
+const getAllEmailQuestions = (filterData) => ({
+  type: GET_ALL_EMAIL_QUESTIONS,
+  payload: filterData
+});
+
+const getEmailQuestionById = (id) => ({
+  type: GET_EMAIL_QUESTION_BY_ID,
+  payload: id
+});
+
+const setCurrentEmailQuestion = (question) => ({
+  type: SET_CURRENT_EMAIL_QUESTION,
+  payload: question
+});
+
+const setEmailQuestionLoading = (loading) => ({
+  type: SET_EMAIL_QUESTIONS_LOADING,
+  payload: loading
+});
+
+const setEmailQuestionsError = (error) => ({
+  type: SET_EMAIL_QUESTIONS_ERROR,
+  payload: error
+});
+
+const makeEmailQuestionSpam = (id) => ({
+  type: MAKE_QUESTION_SPAM,
+  payload: id
+});
+
+const deleteEmailQuestion = (id) => ({
+  type: DELETE_EMAIL_QUESTION,
+  payload: id
+});
+
+const answerToEmailQuestion = (payload) => ({
+  type: ANSWER_TO_EMAIL_QUESTION,
+  payload
+});
+
+export {
+  setAllEmailQuestion,
+  getAllEmailQuestions,
+  getEmailQuestionById,
+  setCurrentEmailQuestion,
+  setEmailQuestionLoading,
+  setEmailQuestionsError,
+  makeEmailQuestionSpam,
+  deleteEmailQuestion,
+  answerToEmailQuestion
+};
