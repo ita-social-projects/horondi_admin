009fddb1b5331c0ff8a47ce9dbe091f609ae230b
Dmytro Didukh <jeriko.outback@gmail.com>
2020-10-16 19:23:21 +0300

    fixed some issues #124

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -1,20 +1,12 @@
-import {
-  differenceInHours,
-  differenceInCalendarDays,
-  formatDistanceToNow,
-  format
-} from 'date-fns';
+import { differenceInHours, differenceInCalendarDays, format } from 'date-fns';
 
 export default (time) => {
   const now = new Date();
   const hoursFromNow = differenceInHours(now, +time);
   const daysFromNow = differenceInCalendarDays(now, +time);
 
-  if (hoursFromNow === 0) {
-    return formatDistanceToNow(+time, { addSuffix: true });
-  } if (hoursFromNow <= 23 && !daysFromNow) {
+  if (hoursFromNow <= 23 && !daysFromNow) {
     return format(+time, 'H:mm');
-  } 
+  }
   return format(+time, 'dd/MM/yy');
-  
 };
