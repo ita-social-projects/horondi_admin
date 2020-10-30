10ece6b19f2f3beae9a75eb056b1a492c190c364
Vova <zerocool1337@ukr.net>
2020-10-30 11:09:47 +0300

    returned back missing files

---
+++ src/redux/dialog-window/dialog-window.reducer.js
@@ -0,0 +1,29 @@
+import { CLOSE_DIALOG, SHOW_DIALOG } from './dialog-window.types';
+
+const initialState = {
+  isOpen: false,
+  dialogTitle: '',
+  dialogContent: '',
+  buttonTitle: '',
+  onClickHandler: () => {},
+  showIcon: true
+};
+
+const dialogWindowReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+  case SHOW_DIALOG:
+    return {
+      ...state,
+      ...action.payload
+    };
+
+  case CLOSE_DIALOG:
+    return {
+      ...initialState
+    };
+  default:
+    return state;
+  }
+};
+
+export default dialogWindowReducer;
