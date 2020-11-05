f033f90972caedea2d9b8196b506166d9c1812cf
unknown <mikhailotorsky@gmail.com>
2020-11-05 22:48:49 +0200

    Redux tests

---
+++ src/redux/home/tests/home.reducer.test.js
@@ -0,0 +1,70 @@
+import {
+  setHomePageData,
+  setHomePageDataLoading,
+  updateHomePageImagesInStore,
+  setHomePageDataError
+} from '../home.actions';
+
+import {
+  images,
+  updatedImages as updatedPhotos,
+  loading,
+  error,
+  id as _id,
+  invalidId,
+  initialState
+} from './home.variables';
+
+import homePageReducer from '../home.reducer';
+
+describe('Homepage reducer tests', () => {
+  it('Should return initial state', () => {
+    expect(homePageReducer()).toEqual(initialState);
+  });
+
+  it('Should set homepage data to store', () => {
+    expect(homePageReducer(initialState, setHomePageData(images))).toEqual({
+      ...initialState,
+      photos: images
+    });
+  });
+
+  it('Should set homepage loading to true', () => {
+    expect(
+      homePageReducer(initialState, setHomePageDataLoading(loading))
+    ).toEqual({
+      ...initialState,
+      homePageLoading: loading
+    });
+  });
+
+  it('Should set homepage error', () => {
+    expect(homePageReducer(initialState, setHomePageDataError(error))).toEqual({
+      ...initialState,
+      homePageError: error
+    });
+  });
+
+  it('Should not update homepage data in store', () => {
+    expect(
+      homePageReducer(
+        initialState,
+        updateHomePageImagesInStore(invalidId, updatedPhotos)
+      )
+    ).toEqual(initialState);
+  });
+
+  it('Should update homepage data in store', () => {
+    initialState.photos.push({ _id });
+
+    expect(
+      homePageReducer(
+        initialState,
+        updateHomePageImagesInStore(_id, updatedPhotos)
+      )
+    ).toEqual({
+      ...initialState,
+      photos: [{ _id }]
+    });
+  });
+});
---
+++ src/redux/home/tests/home.variables.js
@@ -0,0 +1,36 @@
+export const id = '5c3c7929dd85de268bed4fe1';
+export const invalidId = '5c3c7929dd85de268bed4fe2';
+export const images = ['test'];
+export const updatedImage = 'update';
+export const loading = false;
+export const error = false;
+export const updatedImages = { _id: '5c3c7929dd85de268bed4f23' };
+export const initialState = {
+  photos: [],
+  homePageLoading: false,
+  homePageError: null
+};
+
+export const looksImagesMock = {
+  result: {
+    data: {
+      getHomePageLooksImages: [
+        {
+          _id: id,
+          images: null
+        }
+      ]
+    }
+  }
+};
+
+export const looksImageUpdateMock = {
+  result: {
+    data: {
+      updateHomePageLooksImage: {
+        _id: id,
+        images: null
+      }
+    }
+  }
+};
