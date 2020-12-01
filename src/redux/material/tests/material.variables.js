2e6874cdf555ef8f823a413abbf8bb0fd07718b8
Dmytro Malon <dmytro.malonn@gmail.com>
2020-12-02 01:47:59 +0200

    fix sonar

--- src/redux/material/tests/material.variables.js
+++ src/redux/material/tests/material.variables.js
@@ -1,166 +1,6 @@
 export const materialToUpdateDeleteId = '88cb31bf8e5ea5af3014e342';
 export const fakeId = '11cb36bf1e5ea5af3014e312';
 const UA = UA;
-const colorBrown = {
-  available: true,
-  simpleName: [
-    {
-      lang: UA,
-      value: 'коричневий'
-    },
-    {
-      lang: 'en',
-      value: 'brown'
-    }
-  ],
-  images: {
-    medium: 'medium_light-brown'
-  }
-};
-const colorBlue = {
-  available: true,
-  simpleName: [
-    {
-      lang: UA,
-      value: 'синій'
-    },
-    {
-      lang: 'en',
-      value: 'blue'
-    }
-  ],
-  images: {
-    medium: 'medium_steel-blue'
-  }
-};
-const colorGray = {
-  available: true,
-  simpleName: [
-    {
-      lang: UA,
-      value: 'сірий'
-    },
-    {
-      lang: 'en',
-      value: 'grey'
-    }
-  ],
-  images: {
-    medium: 'medium_light-steel'
-  }
-};
-const materialMain = {
-  name: [
-    {
-      lang: UA,
-      value: 'Мальмо'
-    },
-    {
-      lang: 'en',
-      value: 'Malmo'
-    }
-  ],
-  description: [
-    {
-      lang: UA,
-      value:
-        '100% poliester прошита додатковим шаром спеціального матеріалу, який зміцнює та захищає від води + підкладка + фабричний гобелен'
-    },
-    {
-      lang: 'en',
-      value:
-        '100% poliester padded with a layer of durable and water-resistant material + inner layer + factory-made pattern'
-    }
-  ],
-  available: true,
-  additionalPrice: [
-    {
-      value: 0
-    },
-    {
-      value: 0
-    }
-  ],
-  colors: [
-    ...colorBrown,
-    ...colorBrown,
-    ...colorBrown,
-    ...colorBrown,
-    {
-      available: true,
-      simpleName: [
-        {
-          lang: UA,
-          value: 'рожевий'
-        },
-        {
-          lang: 'en',
-          value: 'pink'
-        }
-      ],
-      images: {
-        medium: 'medium_light-pink'
-      }
-    },
-    {
-      available: true,
-      simpleName: [
-        {
-          lang: UA,
-          value: 'червоний'
-        },
-        {
-          lang: 'en',
-          value: 'red'
-        }
-      ],
-      images: {
-        medium: 'medium_violet-red'
-      }
-    },
-    {
-      available: true,
-      simpleName: [
-        {
-          lang: UA,
-          value: 'жовтий'
-        },
-        {
-          lang: 'en',
-          value: 'yellow'
-        }
-      ],
-      images: {
-        medium: 'medium_golden'
-      }
-    },
-    {
-      available: true,
-      simpleName: [
-        {
-          lang: UA,
-          value: 'зелений'
-        },
-        {
-          lang: 'en',
-          value: 'green'
-        }
-      ],
-      images: {
-        medium: 'medium_dark-olive'
-      }
-    },
-    ...colorGray,
-    ...colorBlue,
-    ...colorBlue,
-    ...colorGray,
-    ...colorGray,
-    ...colorGray,
-    ...colorGray,
-    ...colorGray
-  ],
-  purpose: 'main'
-};
 export const material = {
   name: [
     {
@@ -218,11 +58,11 @@ export const materials = [
   },
   {
     _id: '56ade69dd46eafc5968e5390',
-    ...materialMain
+    ...material
   },
   {
     _id: '1d2bba5d0b80938327ac9012',
-    ...materialMain
+    ...material
   }
 ];
 
