4ea1d4f35474be5de2776041c6cab0cb9846217c
Dmytro Malon <dmytro.malonn@gmail.com>
2020-12-03 03:30:27 +0200

    update button

--- src/components/news-form/news-form.js
+++ src/components/news-form/news-form.js
@@ -2,21 +2,30 @@ import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Paper, TextField, Grid, Tab, AppBar, Tabs } from '@material-ui/core';
-import useNewsHandlers from '../../utils/use-news-handlers';
+import {
+  Paper,
+  TextField,
+  Grid,
+  Tab,
+  AppBar,
+  Tabs,
+  Box,
+  Avatar
+} from '@material-ui/core';
+import { Image } from '@material-ui/icons';
 import { useStyles } from './news-form.styles';
+import useNewsHandlers from '../../utils/use-news-handlers';
 import { SaveButton } from '../buttons';
 import TabPanel from '../tab-panel';
-
 import { config } from '../../configs';
 import { updateArticle } from '../../redux/news/news.actions';
+import ImageUploadContainer from '../../containers/image-upload-container';
 
 const { languages } = config;
 
 const NewsForm = ({ article, id }) => {
-  const classes = useStyles();
+  const styles = useStyles();
   const dispatch = useDispatch();
-
   const {
     tabsValue,
     checkboxes,
@@ -25,7 +34,11 @@ const NewsForm = ({ article, id }) => {
     languageCheckboxes,
     setCheckboxes,
     setPreferredLanguages,
-    createArticle
+    createArticle,
+    authorPhoto,
+    setAuthorPhoto,
+    newsImage,
+    setNewsImage
   } = useNewsHandlers();
 
   useEffect(() => {
@@ -55,7 +68,7 @@ const NewsForm = ({ article, id }) => {
       ? preferredLanguages.map((lang, index) => <Tab label={lang} key={lang} />)
       : null;
 
-  const { values, handleSubmit, handleChange } = useFormik({
+  const { values, handleSubmit, handleChange, setFieldValue } = useFormik({
     initialValues: {
       authorPhoto: article.author.image.small || '',
       newsImage: article.images.primary.medium || '',
@@ -72,45 +85,73 @@ const NewsForm = ({ article, id }) => {
     }
   });
 
+  const handleAuthorImageLoad = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setFieldValue('authorPhoto', event.target.result);
+        setAuthorPhoto(event.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  const handleNewsImageLoad = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setFieldValue('newsImage', event.target.result);
+        setNewsImage(event.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div className={classes.controlsBlock}>
+        <div className={styles.controlsBlock}>
           <div>{languageCheckboxes}</div>
           <SaveButton
-            className={classes.saveButton}
+            className={styles.saveButton}
             data-cy='save'
             type='submit'
             title='Зберегти'
           />
         </div>
-        <Grid item xs={12}>
-          <Paper className={classes.newsItemUpdate}>
-            <TextField
-              data-cy='authorPhoto'
-              id='authorPhoto'
-              className={classes.textField}
-              variant='outlined'
-              label='Фото автора'
-              value={values.authorPhoto}
-              onChange={handleChange}
-              required
-            />
-            <TextField
-              data-cy='newsImage'
-              id='newsImage'
-              className={classes.textField}
-              variant='outlined'
-              label='Головне зображення'
-              value={values.newsImage}
-              onChange={handleChange}
-              required
-            />
-          </Paper>
-        </Grid>
+        <Box my={3}>
+          <Grid container spacing={1}>
+            <Grid item>
+              <ImageUploadContainer
+                handler={handleAuthorImageLoad}
+                buttonLabel='Фото Автора'
+              />
+              {authorPhoto && (
+                <Avatar src={authorPhoto}>
+                  <Image />
+                </Avatar>
+              )}
+            </Grid>
+          </Grid>
+        </Box>
+        <Box my={3}>
+          <Grid container spacing={1}>
+            <Grid item>
+              <ImageUploadContainer
+                handler={handleNewsImageLoad}
+                buttonLabel='Головне зображення'
+              />
+              {newsImage && (
+                <Avatar src={newsImage}>
+                  <Image />
+                </Avatar>
+              )}
+            </Grid>
+          </Grid>
+        </Box>
         <AppBar position='static'>
           <Tabs
-            className={classes.tabs}
+            className={styles.tabs}
             value={tabsValue}
             onChange={handleTabsChange}
             aria-label='simple tabs example'
@@ -120,11 +161,11 @@ const NewsForm = ({ article, id }) => {
         </AppBar>
         {preferredLanguages.map((lang, index) => (
           <TabPanel key={index} value={tabsValue} index={index}>
-            <Paper className={classes.newsItemUpdate}>
+            <Paper className={styles.newsItemUpdate}>
               <TextField
                 data-cy={`${lang}AuthorName`}
                 id={`${lang}AuthorName`}
-                className={classes.textField}
+                className={styles.textField}
                 variant='outlined'
                 label={`Ім'я автора`}
                 multiline
@@ -135,7 +176,7 @@ const NewsForm = ({ article, id }) => {
               <TextField
                 data-cy={`${lang}Title`}
                 id={`${lang}Title`}
-                className={classes.textField}
+                className={styles.textField}
                 variant='outlined'
                 label='Заголовок'
                 multiline
@@ -146,7 +187,7 @@ const NewsForm = ({ article, id }) => {
               <TextField
                 data-cy={`${lang}Text`}
                 id={`${lang}Text`}
-                className={classes.textField}
+                className={styles.textField}
                 variant='outlined'
                 label='Текст'
                 multiline
