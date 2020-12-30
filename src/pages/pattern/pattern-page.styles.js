2935ef71f8e060038c42d44d6b07c8cafa2c44c6
Roman <romaniv788@gmail.com>
2020-12-30 19:06:01 +0200

    fix sonar

--- src/pages/pattern/pattern-page.styles.js
+++ src/pages/pattern/pattern-page.styles.js
@@ -1,9 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-  table: {
-    textTransform: 'capitalize'
-  },
   tableNav: {
     display: 'flex',
     justifyContent: 'space-between',
@@ -11,16 +8,19 @@ export const useStyles = makeStyles((theme) => ({
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(2)
   },
-  container: {
-    width: `100%`,
-    marginTop: 65,
-    position: 'static',
-    padding: 20
+  table: {
+    textTransform: 'capitalize'
   },
   paginationDiv: {
     display: 'flex',
     justifyContent: 'center',
     marginTop: '30px'
+  },
+  container: {
+    width: `100%`,
+    marginTop: 65,
+    position: 'static',
+    padding: 20
   },
   patternTitle: {
     marginBottom: '10px',
