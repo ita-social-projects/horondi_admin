6978c9c261c6105568b9b4eef02db84c61272113
Roman <romaniv788@gmail.com>
2021-01-02 15:12:14 +0200

    fix pages reloading

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
 import { CssBaseline } from '@material-ui/core';
+import { ConnectedRouter } from 'connected-react-router';
 import Routes from '../../routes';
 import { theme } from './app-theme/app.theme';
 import { useStyles } from './app.styles';
@@ -9,6 +10,7 @@ import { config } from '../../configs';
 import { checkUserByToken } from '../../redux/auth/auth.actions';
 import { getEmailQuestionsPendingCount } from '../../redux/email-questions/email-questions.actions';
 import { getFromLocalStorage } from '../../services/local-storage.service';
+import { history } from '../../store/store';
 
 const { DARK_THEME, LIGHT_THEME } = config.theme;
 const token = getFromLocalStorage('HORONDI_AUTH_TOKEN');
@@ -29,7 +31,9 @@ const App = () => {
     <ThemeProvider theme={themeValue}>
       <CssBaseline>
         <div className={classes.root}>
-          <Routes />
+          <ConnectedRouter history={history}>
+            <Routes />
+          </ConnectedRouter>
         </div>
       </CssBaseline>
     </ThemeProvider>
