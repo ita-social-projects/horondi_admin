edbe6910d82244e64b1602c4a204ad947630d904
Iryna-Bzdyr <i.bzdyr@outlook.com>
2021-01-16 16:36:47 +0200

    fixed sonar

--- src/pages/model/constructor/constructor-details/constructor-details.js
+++ src/pages/model/constructor/constructor-details/constructor-details.js
@@ -1,25 +1,14 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { useStyles } from './constructor-details.styles';
 import ConstructorForm from '../../../../components/constructor-form';
 import { selectConstructorMethodAndMaterials } from '../../../../redux/selectors/constructor.selectors';
-import { getMaterials } from '../../../../redux/material/material.actions';
 
 const ConstructorDetails = () => {
   const styles = useStyles();
-  const dispatch = useDispatch();
   const {
-    editableConstructorElement,
-    filter
+    editableConstructorElement
   } = useSelector(selectConstructorMethodAndMaterials);
-
-  // useEffect(() => {
-  //   dispatch(
-  //     getMaterials({
-  //       filter
-  //     })
-  //   );
-  // }, [dispatch, filter]);
 
   return (
     <div className={styles.container}>
