fdf0323a6354bdeec83859280ed6aff904ad1451
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-02-09 19:18:48 +0200

    changes

--- src/redux/sizes/sizes.types.js
+++ src/redux/sizes/sizes.types.js
@@ -1,4 +1,3 @@
 export const ADD_SIZE = 'ADD_SIZE';
 export const SET_SIZE_LOADING = 'SET_SIZE_LOADING';
-export const GET_SIZE = 'GET_SIZE';
 export const SAVE_SIZE_IN_REDUX = 'SAVE_SIZE_IN_REDUX';
