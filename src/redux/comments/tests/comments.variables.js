92422c724160ce430a0d5fbdbc9586c59759a248
twihell <sevlost@gmail.com>
2021-03-04 00:53:12 +0200

    add comments-sagas tests

--- src/redux/comments/tests/comments.variables.js
+++ src/redux/comments/tests/comments.variables.js
@@ -15,6 +15,16 @@ export const commentsErrorExample = 'COMMENT_LOADING_ERROR';
 export const commentId = '92cb31bf8e5ea5af3914g342';
 export const productId = '601826s62b8fd28f3bc509e7';
 export const userId = '601d7907des7e48816a42a63';
+export const switchId = 'ed17f080d44d7r88740a4ad1';
+export const mockError = {
+  message: 'COMMENT_LOADING_ERROR'
+};
+
+export const mockSnackbarState = {
+  snackBarStatus: false,
+  snackBarSeverity: '',
+  snackBarMessage: ''
+};
 
 export const pagination = {
   skip: currentPage * rowsPerPage,
@@ -70,4 +80,27 @@ export const singleComment = {
     _id: '601013b62b3fd28f3bc509e7'
   },
   show: true
+};
+
+export const mockTableState = {
+  dense: false,
+  pagination: {
+    currentPage: 0,
+    rowsPerPage: 10,
+    rowsPerPageOptions: [10, 20, 30]
+  },
+  itemsCount: 0
+};
+
+export const commentsInitialState = {
+  list: [],
+  filters: mockInitialFilters,
+  comments: null,
+  commentsLoading: false,
+  commentsError: null
+};
+
+export const commentRes = {
+  items: comments.list,
+  count: 1
 };
