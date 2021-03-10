6574c257bb4939d069ac87b52601312a13035d22
twihell <sevlost@gmail.com>
2021-03-10 20:14:41 +0200

    remove extra lines

--- src/utils/handle-statistic-bar.js
+++ src/utils/handle-statistic-bar.js
@@ -3,9 +3,8 @@ import { Bar } from 'react-chartjs-2';
 
 import StatisticError from '../pages/statistic/statistic-error';
 
-export const handleStatisticBar = (mainData, options) =>
-  mainData.datasets[0].data.length ? (
-    <Bar data={mainData} options={options} redraw />
-  ) : (
-    <StatisticError />
-  );
+export const handleStatisticBar = (mainData, options) => mainData.datasets[0].data.length ? (
+  <Bar data={mainData} options={options} redraw />
+) : (
+  <StatisticError />
+);
