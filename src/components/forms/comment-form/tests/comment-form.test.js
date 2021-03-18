8f77f633e21377c1d72af17ff2b2c191c1fc8527
Ivan Grebiniuk <32039216+grebiniuk@users.noreply.github.com>
2021-03-18 23:10:23 +0000

    Enzyme tests

--- src/components/forms/comment-form/tests/comment-form.test.js
+++ src/components/forms/comment-form/tests/comment-form.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import * as reactRedux from 'react-redux';
-import * as reactRouterDom from 'react-router-dom';
+import * as redux from 'react-redux';
 import { render, fireEvent, act } from '@testing-library/react';
+import * as formik from 'formik';
 
 import CommentForm from '../index';
 import { config } from '../../../../configs';
@@ -13,35 +13,43 @@ configure({ adapter: new Adapter() });
 const { GO_BACK_TITLE, SAVE_TITLE } = config.buttonTitles;
 const { productInfo } = config.labels.comment;
 
+const mockComment = {
+  text: 'Допоможіть! Дитина помалювала фламастером, нічим не можу вивести.',
+  show: true,
+  product: {
+    _id: 'UID'
+  }
+};
+const mockId = '6047321793650236ddbfb841';
+const mockIsEdit = true;
+
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush
+  })
+}));
+
+const mockSetFieldValue = jest.fn();
+jest.mock('formik', () => ({
+  ...jest.requireActual('formik'),
+  useFormik: () => ({
+    values: {},
+    handleSubmit: jest.fn(),
+    handleChange: jest.fn(),
+    touched: {},
+    errors: {},
+    setFieldValue: mockSetFieldValue
+  })
+}));
+
 describe('Comment form tests', () => {
-  const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
-  const mockHistoryPush = jest.fn();
-  jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useHistory: () => ({
-      push: mockHistoryPush
-    })
-  }));
-  const mockSetFieldValue = jest.fn();
-  jest.mock('formik', () => ({
-    ...jest.requireActual('formik'),
-    useFormik: () => ({
-      setFieldValue: mockSetFieldValue
-    })
-  }));
-  const mockComment = {
-    text: 'Допоможіть! Дитина помалювала фламастером, нічим не можу вивести.',
-    show: true,
-    product: {
-      _id: 'UID'
-    }
-  };
-  const mockId = '6047321793650236ddbfb841';
-  const mockIsEdit = true;
+  const mockUseDispatch = jest.spyOn(redux, 'useDispatch');
   let wrapper;
 
   beforeEach(() => {
-    useDispatchMock.mockReturnValue(undefined);
+    mockUseDispatch.mockImplementation(() => jest.fn());
     wrapper = mount(
       <CommentForm comment={mockComment} id={mockId} isEdit={mockIsEdit} />
     );
@@ -49,24 +57,11 @@ describe('Comment form tests', () => {
 
   afterEach(() => {
     wrapper.unmount();
-    useDispatchMock.mockClear();
+    mockUseDispatch.mockClear();
   });
 
   it('Should render component form', () => {
     expect(wrapper.find('form')).toBeDefined();
-  });
-
-  it('Should dispatch update comment if isEdit = true', () => {
-    wrapper.find('form').simulate('submit');
-    expect(useDispatchMock).toHaveBeenCalledTimes(2);
-  });
-
-  it('Should not dispatch update comment if isEdit = false', () => {
-    wrapper = mount(
-      <CommentForm comment={mockComment} id={mockId} isEdit={!mockIsEdit} />
-    );
-    wrapper.find('form').simulate('submit', { preventDefault: () => {} });
-    expect(useDispatchMock).toHaveBeenCalledTimes(3);
   });
 
   it('Should update checkboxes checked value on click', () => {
@@ -90,13 +85,13 @@ describe('Comment form tests', () => {
 
   it(`Should call handleClick Product Info button click`, () => {
     const { getAllByRole } = render(
-      <reactRouterDom.MemoryRouter>
-        <CommentForm comment={mockComment} id={mockId} isEdit={mockIsEdit} />
-      </reactRouterDom.MemoryRouter>
+      <CommentForm comment={mockComment} id={mockId} isEdit={mockIsEdit} />
     );
     fireEvent.click(getAllByRole('button')[0]);
     expect(mockHistoryPush).toHaveBeenCalled();
-    expect(mockHistoryPush).toHaveBeenCalledWith(`/${mockComment.product._id}`);
+    expect(mockHistoryPush).toHaveBeenCalledWith(
+      `/product/${mockComment.product._id}`
+    );
   });
 
   it(`Should render Go back button with '${GO_BACK_TITLE}' label`, () => {
@@ -105,6 +100,11 @@ describe('Comment form tests', () => {
 
   it(`Should render Save button with '${SAVE_TITLE}' label`, () => {
     expect(wrapper.find('button').at(2).text()).toBe(SAVE_TITLE);
+  });
+
+  it('Should have appropriate prop types', () => {
+    expect(CommentForm.propTypes.id).toBe(PropTypes.string);
+    expect(CommentForm.propTypes.isEdit).toBe(PropTypes.bool);
   });
 
   it('Should have default props', () => {
