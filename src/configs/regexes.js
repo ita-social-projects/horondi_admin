e7e8bbfe0147771343f8f02aca2265a0b3852379
dmytro07 <dmytro.furtas07@gmail.com>
2021-03-30 00:54:22 +0300

    Fixed regex

--- src/configs/regexes.js
+++ src/configs/regexes.js
@@ -1,2 +1,2 @@
-export const userNameRegex = /^(?=.{2,30}$)[a-zA-Zа-яА-ЯІЄіїє]+(([',. -][a-zA-Zа-яА-ЯІЄіїє])?[a-zA-Zа-яА-ЯІЄіїє]*)*$/u;
+export const userNameRegex = /^(?=.{2,30}$)[a-zA-Zа-яА-ЯІЄЇіїє]+(([',. -][a-zA-Zа-яА-ЯІЄЇіїє])?[a-zA-Zа-яА-ЯІЄЇіїє]*)*$/u;
 export const phoneNumberRegex = /^\+?3?8?(0\d{9})$/;
