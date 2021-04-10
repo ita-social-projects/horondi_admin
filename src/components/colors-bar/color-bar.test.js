b4522ca6455581bed97ab3fcf0b9485c89d50d5c
Walleriy <walleriystepanenko@gmail.com>
2021-04-10 22:49:08 +0300

    feat: add component rendering

--- src/components/colors-bar/color-bar.test.js
+++ src/components/colors-bar/color-bar.test.js
@@ -2,45 +2,49 @@ import React from 'react';
 import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Provider } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ColorsBar from './colors-bar';
 import configureStore from '../../store/store';
+import ColorsAutocomplete from '../colors-autocomplete';
+import DialogWindowWrapper from '../dialog-window-wrapper';
+import CreateColor from '../create-color';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('should render colors-bar component', () => {
-  const colors = [
-    {
-      color: 'red'
-    },
-    {
-      color: 'white'
-    }
-  ];
-  const fu = () => {
-    const a = 5;
-    return a;
-  };
+const store = configureStore();
 
-  const store = configureStore();
+const setUpComponent = (props) =>
+  mount(
+    <Provider store={store}>
+      <ColorsBar {...props} />
+    </Provider>
+  );
 
+describe('Colors-Bar test', () => {
   let component;
 
   beforeEach(() => {
-    component = mount(
-      <Provider store={store}>
-        <ColorsBar />
-      </Provider>
-    );
-    console.log('///////////////////////////////////////////////////////////');
-    console.log(component.debug());
-
-    console.log('///////////////////////////////////////////////////////////');
-    console.log('///////////////////////////////////////////////////////////');
+    component = setUpComponent();
+    // console.log(component.debug());
   });
 
-  it('should exist', () => {
-    // expect(component).toMatchSnapshot();
-    expect(component.exists());
+  afterEach(() => {
+    component.unmount();
+  });
+
+  it('Should render Colors-Bar', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('Should render ColorsAutocomplete', () => {
+    expect(component.exists(ColorsAutocomplete)).toBe(true);
+  });
+  it('Should render Button', () => {
+    expect(component.exists(Button)).toBe(true);
+  });
+  it('Should render 2 DialogWindowWrapper', () => {
+    expect(component.find(DialogWindowWrapper).length).toBe(2);
   });
 
   /* it('should have props', () => {
