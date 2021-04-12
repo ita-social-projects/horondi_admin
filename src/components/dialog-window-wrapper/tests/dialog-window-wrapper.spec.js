611ef9bdd31df682db501f0c174ac3ebc6762f48
100vbaSasha <stovbasasha@gmail.com>
2021-04-12 17:49:47 +0300

    small refactoring

--- src/components/dialog-window-wrapper/tests/dialog-window-wrapper.spec.js
+++ src/components/dialog-window-wrapper/tests/dialog-window-wrapper.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Dialog, DialogContent, DialogTitle, Tooltip } from '@material-ui/core';
@@ -9,51 +10,62 @@ Enzyme.configure({ adapter: new Adapter() });
 
 const setUp = (props) => shallow(<DialogWindowWrapper {...props} />);
 
-describe('should render DialogWindowWrapper component', () => {
+describe('Dialog Window Wrapper component', () => {
   let component;
   beforeEach(() => {
     component = setUp(testProps);
   });
-  it('should contain Dialog', () => {
+  it('Should contain Dialog', () => {
     const dialog = component.find(Dialog);
     expect(dialog.length).toBe(1);
   });
-  it('should contain DialogContent', () => {
+  it('Should contain DialogContent', () => {
     const dialogContent = component.find(DialogContent);
     expect(dialogContent.length).toBe(1);
   });
-  it('should contain DialogContent', () => {
+  it('Should contain DialogContent', () => {
     const dialogTitle = component.find(DialogTitle);
     expect(dialogTitle.length).toBe(1);
   });
-  it('should contain DialogContent', () => {
+  it('Should contain DialogContent', () => {
     const tooltip = component.find(Tooltip);
     expect(tooltip.length).toBe(1);
   });
 });
 
-describe('Props testing', () => {
+describe('Dialog Window Wrapper component props', () => {
   const component = mount(<DialogWindowWrapper {...testProps} />);
 
-  it('test props: isOpen', () => {
+  it('Should be valid', () => {
     expect(component.props().isOpen).toEqual(true);
     expect(component.props().isOpen).toBeDefined();
     expect(component.props().isOpen).not.toBeNull();
     expect(component.props().isOpen).toEqual(testProps.isOpen);
-  });
-  it('test props: title', () => {
+
     expect(component.props().title).toBeDefined();
     expect(component.props().title).not.toBeNull();
     expect(component.props().title).toEqual(testProps.title);
-  });
-  it('test props: handleClose', () => {
+
     expect(component.props().handleClose).toBeDefined();
     expect(component.props().handleClose).not.toBeNull();
     expect(component.props().handleClose).toEqual(testProps.handleClose);
-  });
-  it('test props: children', () => {
+
     expect(component.props().children).toBeDefined();
     expect(component.props().children).toBeNull();
     expect(component.props().children).toEqual(null);
+  });
+  it('Should have default props', () => {
+    expect(DialogWindowWrapper.defaultProps.children).toBe(null);
+  });
+  it('Should have prop types', () => {
+    expect(DialogWindowWrapper.propTypes.isOpen).toBe(
+      PropTypes.bool.isRequired
+    );
+    expect(DialogWindowWrapper.propTypes.handleClose).toBe(
+      PropTypes.func.isRequired
+    );
+    expect(DialogWindowWrapper.propTypes.title).toBe(
+      PropTypes.string.isRequired
+    );
   });
 });
