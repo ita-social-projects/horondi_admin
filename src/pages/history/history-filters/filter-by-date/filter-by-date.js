79a258e15487fb75f4fdc2b599208d7b6e3bf714
AndriyLymych <lymych1994@gmail.com>
2021-04-12 13:34:52 +0300

    add filter

--- src/pages/history/history-filters/filter-by-date/filter-by-date.js
+++ src/pages/history/history-filters/filter-by-date/filter-by-date.js
@@ -1,29 +1,29 @@
-import * as React from 'react';
-import TextField from '@material-ui/core/TextField';
-import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
-import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
-import Box from '@material-ui/core/Box';
-import DesktopDateRangePicker from '@material-ui/lab/DesktopDateRangePicker';
-
-export default function FilterByDate() {
-  const [value, setValue] = React.useState([null, null]);
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DesktopDateRangePicker
-        startText="Desktop start"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(startProps, endProps) => (
-          <React.Fragment>
-            <TextField {...startProps} variant="standard" />
-            <Box sx={{ mx: 2 }}> to </Box>
-            <TextField {...endProps} variant="standard" />
-          </React.Fragment>
-        )}
-      />
-    </LocalizationProvider>
-  );
-}
+// import * as React from 'react';
+// import TextField from '@material-ui/core/TextField';
+// import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+// import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+// import Box from '@material-ui/core/Box';
+// import DesktopDateRangePicker from '@material-ui/lab/DesktopDateRangePicker';
+//
+// export default function FilterByDate() {
+//   const [value, setValue] = React.useState([null, null]);
+//
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDateFns}>
+//       <DesktopDateRangePicker
+//         startText="Desktop start"
+//         value={value}
+//         onChange={(newValue) => {
+//           setValue(newValue);
+//         }}
+//         renderInput={(startProps, endProps) => (
+//           <React.Fragment>
+//             <TextField {...startProps} variant="standard" />
+//             <Box sx={{ mx: 2 }}> to </Box>
+//             <TextField {...endProps} variant="standard" />
+//           </React.Fragment>
+//         )}
+//       />
+//     </LocalizationProvider>
+//   );
+// }
