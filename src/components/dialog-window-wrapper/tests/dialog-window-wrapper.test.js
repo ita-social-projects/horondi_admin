78897536578033fe28de6e72399cfeee8221fb20
100vbaSasha <stovbasasha@gmail.com>
2021-04-13 11:45:55 +0300

    added variables file, rename test file

--- src/components/dialog-window-wrapper/tests/dialog-window-wrapper.test.js
+++ src/components/dialog-window-wrapper/tests/dialog-window-wrapper.test.js
@@ -4,14 +4,9 @@ import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Dialog, DialogContent, DialogTitle, Tooltip } from '@material-ui/core';
 import DialogWindowWrapper from '../dialog-window-wrapper';
+import { testProps } from './dialog-window-wrapper.variables';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-const testProps = {
-  isOpen: true,
-  handleClose: jest.fn(),
-  title: 'Title'
-};
 
 const setUp = (props) => shallow(<DialogWindowWrapper {...props} />);
 
