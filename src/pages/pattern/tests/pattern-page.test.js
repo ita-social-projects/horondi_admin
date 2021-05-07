9e4cdf4ab92c47f4e5b854939f5c549067f9778a
MiroslavMashivskyi <62468496+MiroslavMashivskyi@users.noreply.github.com>
2021-05-07 14:07:35 +0300

    Changing branch

--- src/pages/pattern/tests/pattern-page.test.js
+++ src/pages/pattern/tests/pattern-page.test.js
@@ -1,75 +1,187 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { configure, mount } from 'enzyme';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { configure, shallow, mount, to, be } from 'enzyme';
+import { Link, BrowserRouter } from 'react-router-dom';
 import * as reactRedux from 'react-redux';
+import { useDispatch as useDispatchMock } from 'react-redux';
 import { Button, Typography } from '@material-ui/core';
-import { variables } from './variables';
+import mockStore from './__mock__';
 
 import PatternPage from '../pattern-page';
 import LoadingBar from '../../../components/loading-bar';
 import TableContainerGenerator from '../../../containers/table-container-generator';
+import TableContainerRow from '../../../containers/table-container-row';
 
 import { config } from '../../../configs';
 
 const { CREATE_PATTERN_TITLE } = config.buttonTitles;
 const pathToPatternAddPage = config.routes.pathToAddPattern;
+const tableTitles = config.tableHeadRowTitles.patterns;
 
 configure({ adapter: new Adapter() });
-const mockStore = variables;
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
-  useSelector: (selector) => selector(mockStore),
-  useDispatch: () => jest.fn()
-}));
 
-describe('Pattern-page tests', () => {
-  jest.spyOn(reactRedux, 'useDispatch');
-  const mockHolder = jest.fn();
+// describe('Pattern-page render tests', () => {
+//   let spyOnUseSelector;
+//   let spyOnUseDispatch;
+//   let mockDispatch;
+//   let wrapper;
+//   let typography;
+//   let button;
+//   let tableContainerGenerator;
+//   let getState;
+
+// beforeEach(() => {
+//   // Mock useSelector hook
+//   spyOnUseSelector = jest.spyOn(reactRedux, 'useSelector');
+//   spyOnUseSelector.mockImplementation(()=> mockStore);
+
+//   // Mock useEffect
+
+//   // Mock useDispatch hook
+//   spyOnUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
+//   // Mock dispatch function returned from useDispatch
+//   mockDispatch = jest.fn();
+//   spyOnUseDispatch.mockReturnValue(mockDispatch);
+
+//   wrapper = shallow(<PatternPage />);
+//   typography = wrapper.find(Typography);
+//   button = wrapper.find(Button);
+//   tableContainerGenerator = wrapper.find(TableContainerGenerator);
+// });
+
+//   afterEach(() => {
+//     jest.restoreAllMocks();
+//     spyOnUseSelector.mockClear();
+//   });
+
+//   test('Should render pattern-page', () => {
+//     expect(wrapper).toBeDefined();
+//     expect(wrapper).toHaveLength(1);
+//     expect(wrapper).toMatchSnapshot();
+//   });
+
+//   test(`Should render Typography wrapper with "${config.titles.patternTitles.mainPageTitle}" label`, () => {
+//     expect(wrapper.exists(Typography)).toBe(true);
+//     expect(typography).toHaveLength(1);
+//     expect(typography.text()).toBe(config.titles.patternTitles.mainPageTitle);
+//   });
+
+//   test(`Should render button wrapper`, () => {
+//     expect(wrapper.exists(Button)).toBe(true);
+//     expect(button).toHaveLength(1);
+//     expect(button.text()).toBe(CREATE_PATTERN_TITLE);
+//     expect(button.prop('component')).toEqual(Link);
+//     expect(button.prop('to')).toBe(pathToPatternAddPage);
+//   });
+
+//   test('Should render TableContainerGenerator', () => {
+//     expect(wrapper.exists(TableContainerGenerator)).toBe(true);
+//     expect(tableContainerGenerator).toHaveLength(1);
+//     expect(wrapper.exists(LoadingBar)).toBe(false);
+//     expect(tableContainerGenerator.prop('tableTitles')).toBe(tableTitles);
+//   });
+
+//   test('Should render LoadingBar', () => {
+//     mockStore.loading = true;
+//     wrapper = shallow(<PatternPage />);
+//     const loadingBar = wrapper.find(LoadingBar);
+//     expect(wrapper.exists(LoadingBar)).toBeDefined();
+//     expect(wrapper.exists(LoadingBar)).toBe(true);
+//     expect(loadingBar).toHaveLength(1);
+//     expect(wrapper.exists(TableContainerGenerator)).toBe(false);
+//     mockStore.loading = false;
+//   });
+
+//   test('useSelector hook should be called', () => {
+//     getState = reactRedux.useSelector(mockStore);
+//     expect(getState).toEqual(mockStore);
+//     expect(spyOnUseSelector).toHaveBeenCalled();
+//   })
+// });
+
+describe('useEffect tests', () => {
+  let spyOnUseSelector;
+  let spyOnUseDispatch;
+  let mockDispatch;
   let wrapper;
+  let patternPage;
+  let instance;
+
+  let tableContainerRow;
+  let tableContainerRowFirst;
+  let tableContainerRowSecond;
+  let tableContainerRowThird;
 
   beforeEach(() => {
+    // Mock useSelector hook
+    spyOnUseSelector = jest.spyOn(reactRedux, 'useSelector');
+    spyOnUseSelector.mockImplementation(() => mockStore);
+
+    // Mock useDispatch hook
+    spyOnUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
+    // Mock dispatch function returned from useDispatch
+    mockDispatch = jest.fn();
+    spyOnUseDispatch.mockReturnValue(mockDispatch);
+
     wrapper = mount(
       <BrowserRouter>
         <PatternPage />
       </BrowserRouter>
     );
+
+    patternPage = wrapper.find(PatternPage);
+    instance = patternPage.instance();
+    console.log(instance);
+
+    tableContainerRow = patternPage.find(TableContainerRow);
+    tableContainerRowFirst = patternPage.find({
+      id: '6043b87c3e06ad3edcdb7b19'
+    });
+    tableContainerRowSecond = patternPage.find({
+      id: '6043b8c53e06ad3edcdb7b1a'
+    });
+    tableContainerRowThird = patternPage.find({
+      id: '6043b90d3e06ad3edcdb7b1b'
+    });
   });
 
   afterEach(() => {
     wrapper.unmount();
+    jest.restoreAllMocks();
+    spyOnUseSelector.mockClear();
   });
 
-  test('Does page render', () => {
-    expect(wrapper).toBeDefined();
-  });
+  // test('UseEffect hook shoud work out', () => {
+  //   expect(spyOnUseDispatch).toHaveBeenCalledTimes(2);
+  // })
 
-  test(`Should render Typography component with "${config.titles.patternTitles.mainPageTitle}" label`, () => {
-    expect(wrapper.exists(Typography)).toBe(true);
-    expect(wrapper.find(Typography).text()).toBe(
-      config.titles.patternTitles.mainPageTitle
-    );
-  });
+  // test('Should render TableContainerRow', () => {
+  //   expect(patternPage.exists(TableContainerRow)).toBe(true);
+  //   expect(tableContainerRow).toHaveLength(3);
+  //   expect(tableContainerRowFirst.prop('available')).toBe('Так');
+  //   expect(tableContainerRowSecond.prop('available')).toBe('Так');
+  //   expect(tableContainerRowThird.prop('available')).toBe('Так');
+  //   expect(tableContainerRowFirst.prop('name')).toBe('Червоний');
+  //   expect(tableContainerRowSecond.prop('name')).toBe('Сірий квадрат');
+  //   expect(tableContainerRowThird.prop('name')).toBe('Голуба стрічка');
+  // });
 
-  test(`Should render button component`, () => {
-    expect(wrapper.exists(Button)).toBe(true);
-    expect(wrapper.find(Button).text()).toBe(CREATE_PATTERN_TITLE);
-    expect(wrapper.find(Button).prop('component')).toEqual(Link);
-    expect(wrapper.find(Button).prop('to')).toBe(pathToPatternAddPage);
+  test('aaaa', () => {
+    expect(instance).to.be.instanceOf(PatternPage);
   });
+  // test('Should handle click the Delete pattern button', () => {
+  // useDispatchMock.mockReturnValue(mockDispatch);
+  // patternPage.find(TableContainerRow).at(0).props().deleteHandler();
+  // expect(mockDispatch).toHaveBeenCalledTimes(4);
+  // });
 
-  test('Should render LoadingBar', () => {
-    mockStore.Pattern.patternLoading = true;
-    wrapper = mount(
-      <BrowserRouter>
-        <PatternPage />
-      </BrowserRouter>
-    );
-    mockStore.Pattern.patternLoading = false;
-    expect(wrapper.exists(LoadingBar)).toBe(true);
-  });
-
-  test('Should render TableContainerGenerator', () => {
-    expect(wrapper.exists(TableContainerGenerator)).toBe(true);
-  });
+  // test('Should handle click the Edit pattern button', () => {
+  //   patternPage.find(TableContainerRow).at(0).props().editHandler();
+  //   expect(spyOnUseDispatch).toHaveBeenCalledTimes(3);
+  // });
+  // test('useEffect should work', () => {
+  // console.log(wrapper.find('PatternPage').find('TableContainerRow').debug());
+  //   let spyOnUseEffect = jest.spyOn(React, 'useEffect');
+  //   expect(spyOnUseEffect).toHaveBeenCalled();
+  // })
 });
