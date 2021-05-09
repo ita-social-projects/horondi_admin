8c8264c3fc19500715143e418230837fc17f05cb
Yurii <yur.dub7@gmail.com>
2021-05-09 15:52:17 +0300

    add unit tests and solution for jest config bug

--- src/components/forms/material-form/tests/material-form.test.js
+++ src/components/forms/material-form/tests/material-form.test.js
@@ -1,18 +1,35 @@
 import React from 'react';
-
-import { configure, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as redux from 'react-redux';
+import Select from '@material-ui/core/Select';
+import LanguagePanel from '../../language-panel';
+import { BackButton, SaveButton } from '../../../buttons';
+import LoadingBar from '../../../loading-bar';
 import MaterialForm from '../index';
+import { id, material, defaultProps } from './material-form.variables';
 
 configure({ adapter: new Adapter() });
 
+const mockCreateMaterial = jest.fn();
+
+jest.mock('../../../../utils/use-material-handlers', () => ({
+  __esModule: true,
+  default: () => ({
+    createMaterial: mockCreateMaterial
+  })
+}));
+
 const mockSetFieldValue = jest.fn();
+const mockSubmit = jest.fn();
+
 jest.mock('formik', () => ({
   ...jest.requireActual('formik'),
   useFormik: () => ({
-    values: {},
-    handleSubmit: jest.fn(),
+    values: {
+      purpose: 'CLOSURE'
+    },
+    handleSubmit: mockSubmit,
     handleChange: jest.fn(),
     touched: {},
     errors: {},
@@ -24,17 +41,73 @@ describe('Material form tests', () => {
   const mockUseDispatch = jest.spyOn(redux, 'useDispatch');
   const mockUseSelector = jest.spyOn(redux, 'useSelector');
   let wrapper;
+
   beforeEach(() => {
     mockUseDispatch.mockImplementation(() => jest.fn());
-    // mockUseSelector.mockImplementation(() => false);
-    wrapper = mount(<MaterialForm />);
+    mockUseSelector.mockReturnValue({ loading: false });
+    wrapper = shallow(<MaterialForm id={id} material={material} />);
   });
+
   afterEach(() => {
     wrapper.unmount();
     mockUseDispatch.mockClear();
-    // mockUseSelector.mockClear();
+    mockUseSelector.mockClear();
   });
+
+  it('Should render LoadingBar', () => {
+    mockUseSelector.mockReturnValue({ loading: true });
+    wrapper = shallow(<MaterialForm id={id} material={material} />);
+    expect(wrapper.exists(LoadingBar)).toBe(true);
+  });
+
   it('Should render component form', () => {
-    expect(wrapper.find('form')).toHaveLength(1);
+    expect(wrapper.exists('form')).toBe(true);
+  });
+
+  it('Should render two LanguagePanel components', () => {
+    expect(wrapper.find(LanguagePanel)).toHaveLength(2);
+  });
+
+  it('Should render BackButton and SaveButton components', () => {
+    expect(wrapper.exists(BackButton)).toBe(true);
+    expect(wrapper.exists(SaveButton)).toBe(true);
+  });
+
+  it('Should simulate click event on CheckboxOptions', () => {
+    wrapper.find('CheckboxOptions').props().options[0].handler();
+    expect(mockSetFieldValue).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should simulate click event on ColorBar', () => {
+    wrapper.find('ColorsBar').props().onColorChange(material.colors);
+    expect(mockSetFieldValue).toHaveBeenCalledTimes(2);
+  });
+
+  it(`Should have ${material.purpose} purpose value props`, () => {
+    expect(wrapper.find(Select).props().value).toBe(material.purpose);
+  });
+
+  it('Should simulate onchange click event on Select', () => {
+    wrapper
+      .find(Select)
+      .props()
+      .onChange({ target: { value: 'PATTERN' } });
+    expect(mockSetFieldValue).toHaveBeenCalledTimes(3);
+    expect(mockSetFieldValue).toHaveBeenNthCalledWith(3, 'purpose', 'PATTERN');
+  });
+
+  it('Should simulate submit event', () => {
+    wrapper.find('form').props().onSubmit();
+    expect(mockSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should have default props', () => {
+    expect(MaterialForm.defaultProps).toBeDefined();
+    expect(MaterialForm.defaultProps.id).toBe(defaultProps.id);
+    expect(MaterialForm.defaultProps.values).toEqual(defaultProps.values);
+    expect(MaterialForm.defaultProps.errors).toEqual(defaultProps.errors);
+    expect(MaterialForm.defaultProps.touched).toEqual(defaultProps.touched);
+    expect(MaterialForm.defaultProps.match).toEqual(defaultProps.match);
+    expect(MaterialForm.defaultProps.material).toEqual(defaultProps.material);
   });
 });
