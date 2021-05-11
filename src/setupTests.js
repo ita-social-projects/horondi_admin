c2cbfbc8f4b6a6dfb7e9aae65fb15a364b508628
Serg Klukvin <sergklukvin@gmail.com>
2021-05-11 15:47:04 +0300

    delete unnecessary changes

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,6 @@ import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
-
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -11,5 +10,3 @@ window.shallow = shallow;
 window.render = render;
 window.mount = mount;
 window.toJson = toJson;
-
-
