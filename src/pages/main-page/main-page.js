55113c59f4efce541a07ef55fed173c95e3319bc
Serg Klukvin <sergklukvin@gmail.com>
2021-05-11 17:22:06 +0300

    delete unnecessary changes v1.3

--- src/pages/main-page/main-page.js
+++ src/pages/main-page/main-page.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 import moment from 'moment';
@@ -22,7 +22,7 @@ import routes from '../../configs/routes';
 
 import {useCommonStyles} from '../common.styles';
 import {useStyles} from './main-page.styles';
-import {getEmailQuestionsPendingCount} from '../../redux/email-questions/email-questions.actions';
+import {getEmailQuestionsPendingCount} from "../../redux/email-questions/email-questions.actions";
 
 const map = require('lodash/map');
 
@@ -39,116 +39,115 @@ const MainPage = () => {
 
     const {orderLoading, ordersList} = useSelector(selectOrderList);
     const {rowsPerPage, currentPage} = useSelector(
-      commentSelectorWithPagination
-  );
-
-  useEffect(() => {
-    dispatch(
-      getRecentComments({
-        pagination: {
-          limit: rowsPerPage,
-          skip: currentPage * rowsPerPage
-        }
-      })
+        commentSelectorWithPagination
     );
-  }, [dispatch, rowsPerPage, currentPage]);
 
-  useEffect(() => {
-    dispatch(
-      getOrderList({
-        limit: rowsPerPage,
-        skip: currentPage * rowsPerPage,
-        filter: {
-          orderStatus: 'CREATED'
-        }
-      })
+    useEffect(() => {
+        dispatch(
+            getRecentComments({
+                pagination: {
+                    limit: rowsPerPage,
+                    skip: currentPage * rowsPerPage
+                }
+            })
+        );
+    }, [dispatch, rowsPerPage, currentPage]);
+
+    useEffect(() => {
+        dispatch(
+            getOrderList({
+                limit: rowsPerPage,
+                skip: currentPage * rowsPerPage,
+                filter: {
+                    orderStatus: 'CREATED'
+                }
+            })
+        );
+    }, [dispatch, rowsPerPage, currentPage]);
+
+    useEffect(() => {
+        dispatch(getEmailQuestionsPendingCount());
+    }, []);
+
+    if (orderLoading || loading) {
+        return <LoadingBar/>;
+    }
+
+    const comments = map(list, ({date, text, user, _id}) => (
+        <div key={_id} className={classes.comment}>
+            <div className={classes.commentText}>{text}</div>
+            <div className={classes.commentInfo}>
+                <div>{user?.firstName || guestUser}</div>
+                <div>
+                    <div>{moment.unix(date / 1000).format('HH:mm')}</div>
+                    <div>{moment.unix(date / 1000).format('DD.MM.YYYY ')}</div>
+                </div>
+            </div>
+        </div>
+    ));
+
+    const orders =
+        ordersList && ordersList.length
+            ? map(ordersList, ({dateOfCreation, totalItemsPrice, _id}) => (
+                <TableRow
+                    key={_id}
+                    onClick={() => dispatch(push(`${pathToOrders}/${_id}`))}
+                    className={classes.order}
+                    data-cy='order'
+                >
+                    <TableCell>
+                        {moment.unix(dateOfCreation / 1000).format('DD.MM.YYYY')}
+                    </TableCell>
+                    <TableCell>
+                        {totalItemsPrice[0].value}
+                        {totalItemsPrice[0].currency} / {totalItemsPrice[1].value}
+                        {totalItemsPrice[1].currency}
+                    </TableCell>
+                    <TableCell>{_id}</TableCell>
+                </TableRow>
+            ))
+            : null;
+
+    return (
+        <div className={`${commonClasses.container} ${classes.root}`}>
+            <Typography
+                variant='h3'
+                className={commonClasses.materialTitle}
+                data-cy='page-title'
+            >
+                {mainTitle}
+            </Typography>
+            <div className={classes.main}>
+                <div className={classes.commentsOrders}>
+                    <Paper className={classes.ordersContainer} data-cy='orders-container'>
+                        <Typography variant='h5' className={classes.blockTitle}>
+                            {ordersTitle}
+                        </Typography>
+                        {ordersList && ordersList.length ? (
+                            <TableContainerGenerator
+                                tableItems={orders}
+                                tableTitles={ordersTableTitles}
+                                data-cy='orders-table'
+                            />
+                        ) : (
+                            <div className={classes.emptyList} data-cy='empty-orders'>
+                                {EMPTY_LIST}
+                            </div>
+                        )}
+                    </Paper>
+                    <Paper
+                        className={classes.commentsContainer}
+                        data-cy='comments-container'
+                    >
+                        <Typography variant='h5' className={classes.blockTitle}>
+                            {commentsTitle}
+                        </Typography>
+                        <div className={classes.comments}>{comments}</div>
+                    </Paper>
+                </div>
+            </div>
+        </div>
     );
-  }, [dispatch, rowsPerPage, currentPage]);
-
-  useEffect(() => {
-    dispatch(getEmailQuestionsPendingCount());
-  }, []);
-
-  if (orderLoading || loading) {
-    return <LoadingBar />;
-  }
-
-  const comments = map(list, ({ date, text, user, _id }) => (
-    <div key={_id} className={classes.comment}>
-      <div className={classes.commentText}>{text}</div>
-      <div className={classes.commentInfo}>
-        <div>{user?.firstName || guestUser}</div>
-        <div>
-          <div>{moment.unix(date / 1000).format('HH:mm')}</div>
-          <div>{moment.unix(date / 1000).format('DD.MM.YYYY ')}</div>
-        </div>
-      </div>
-    </div>
-  ));
-
-  const orders =
-    ordersList && ordersList.length
-      ? map(ordersList, ({ dateOfCreation, totalItemsPrice, _id }) => (
-          <TableRow
-            key={_id}
-            onClick={() => dispatch(push(`${pathToOrders}/${_id}`))}
-            className={classes.order}
-            data-cy='order'
-          >
-            <TableCell>
-              {moment.unix(dateOfCreation / 1000).format('DD.MM.YYYY')}
-            </TableCell>
-            <TableCell>
-              {totalItemsPrice[0].value}
-              {totalItemsPrice[0].currency} / {totalItemsPrice[1].value}
-              {totalItemsPrice[1].currency}
-            </TableCell>
-            <TableCell>{_id}</TableCell>
-          </TableRow>
-        ))
-      : null;
-
-  return (
-    <div className={`${commonClasses.container} ${classes.root}`}>
-      <Typography
-        variant='h3'
-        className={commonClasses.materialTitle}
-        data-cy='page-title'
-      >
-        {mainTitle}
-      </Typography>
-      <div className={classes.main}>
-        <div className={classes.commentsOrders}>
-          <Paper className={classes.ordersContainer} data-cy='orders-container'>
-            <Typography variant='h5' className={classes.blockTitle}>
-              {ordersTitle}
-            </Typography>
-
-            {ordersList && ordersList.length ? (
-              <TableContainerGenerator
-                tableItems={orders}
-                tableTitles={ordersTableTitles}
-                data-cy='orders-table'
-              />
-            ) : (
-              <div className={classes.emptyList} data-cy='empty-orders'>
-                {EMPTY_LIST}
-              </div>
-            )}
-          </Paper>
-          <Paper
-            className={classes.commentsContainer}
-            data-cy='comments-container'
-          >
-            <Typography variant='h5' className={classes.blockTitle}>
-              {commentsTitle}
-            </Typography>
-            <div className={classes.comments}>{comments}</div>
-          </Paper>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default MainPage;
