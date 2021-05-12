8c8890feff306f0ac3fb1fe40fae87c6052b9c09
AndriyLymych <lymych1994@gmail.com>
2021-05-12 08:22:31 +0300

    fix sort

--- src/pages/orders/orders-page.js
+++ src/pages/orders/orders-page.js
@@ -1,151 +1,152 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {push} from 'connected-react-router';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { push } from 'connected-react-router';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ReactHtmlParser from 'react-html-parser';
 
 import orders from '../../configs/orders';
 import getTime from '../../utils/getTime';
-import {useStyles} from './orders-page.styles';
-import {useCommonStyles} from '../common.styles';
-import {getOrderList, deleteOrder} from '../../redux/orders/orders.actions';
+import { useStyles } from './orders-page.styles';
+import { useCommonStyles } from '../common.styles';
+import { getOrderList, deleteOrder } from '../../redux/orders/orders.actions';
 import Status from './Status/Status';
 import LoadingBar from '../../components/loading-bar';
 import TableContainerRow from '../../containers/table-container-row';
-import {config} from '../../configs';
+import { config } from '../../configs';
 import useSuccessSnackbar from '../../utils/use-success-snackbar';
-import {closeDialog} from '../../redux/dialog-window/dialog-window.actions';
-import {handleOrdersPage} from '../../utils/handle-orders-page';
+import { closeDialog } from '../../redux/dialog-window/dialog-window.actions';
+import { handleOrdersPage } from '../../utils/handle-orders-page';
 import useOrderFilters from '../../hooks/filters/use-order-filters';
 import FilterNavbar from '../../components/filter-search-sort';
 
-const {ADD_ORDER} = config.buttonTitles;
+const { ADD_ORDER } = config.buttonTitles;
 const pathToOrdersAddPage = config.routes.pathToOrderAdd;
-const {pathToOrderItem} = config.routes;
-const {REMOVE_ORDER_MESSAGE} = config.messages;
+const { pathToOrderItem } = config.routes;
+const { REMOVE_ORDER_MESSAGE } = config.messages;
 
 const OrdersPage = () => {
-    const styles = useStyles();
-    const commonStyles = useCommonStyles();
-    const dispatch = useDispatch();
-    const {openSuccessSnackbar} = useSuccessSnackbar();
+  const styles = useStyles();
+  const commonStyles = useCommonStyles();
+  const dispatch = useDispatch();
+  const { openSuccessSnackbar } = useSuccessSnackbar();
 
-    const {
-        searchOptions,
-        clearOptions,
-        filterByMultipleOptions,
-        filterByDateOptions,
-        sortOptions
-    } = useOrderFilters();
+  const {
+    searchOptions,
+    clearOptions,
+    filterByMultipleOptions,
+    filterByDateOptions,
+    sortOptions
+  } = useOrderFilters();
 
-    const {orderLoading, orders: ordersList, filters, sort} = useSelector(
-        ({Orders}) => ({
-            orderLoading: Orders.orderLoading,
-            orders: Orders.list.items,
-            filters: Orders.filters,
-            sort: Orders.sort
-        })
+  const { orderLoading, orders: ordersList, filters, sort } = useSelector(
+    ({ Orders }) => ({
+      orderLoading: Orders.orderLoading,
+      orders: Orders.list.items,
+      filters: Orders.filters,
+      sort: Orders.sort
+    })
+  );
+
+  const { currentPage, rowsPerPage, itemsCount } = useSelector(({ Table }) => ({
+    currentPage: Table.pagination.currentPage,
+    rowsPerPage: Table.pagination.rowsPerPage,
+    itemsCount: Table.itemsCount
+  }));
+
+  useEffect(() => {
+    dispatch(
+      getOrderList({
+        limit: rowsPerPage,
+        skip: currentPage * rowsPerPage,
+        filter: {
+          date: { dateFrom: filters.dateFrom, dateTo: filters.dateTo },
+          status: filters.status,
+          paymentStatus: filters.paymentStatus,
+          search: filters.search
+        },
+        sort
+      })
     );
-    const {currentPage, rowsPerPage, itemsCount} = useSelector(({Table}) => ({
-        currentPage: Table.pagination.currentPage,
-        rowsPerPage: Table.pagination.rowsPerPage,
-        itemsCount: Table.itemsCount
-    }));
+  }, [dispatch, rowsPerPage, currentPage, filters, sort]);
 
-    useEffect(() => {
-        dispatch(
-            getOrderList({
-                limit: rowsPerPage,
-                skip: currentPage * rowsPerPage,
-                filter: {
-                    date: {dateFrom: filters.dateFrom, dateTo: filters.dateTo},
-                    status: filters.status,
-                    paymentStatus: filters.paymentStatus,
-                    search: filters.search
-                },
-                sort
-            })
-        );
-    }, [dispatch, rowsPerPage, currentPage, filters, sort]);
-
-    const ordersDeleteHandler = (id) => {
-        const removeOrders = () => {
-            dispatch(closeDialog());
-            dispatch(deleteOrder(id));
-        };
-        openSuccessSnackbar(removeOrders, REMOVE_ORDER_MESSAGE);
+  const ordersDeleteHandler = (id) => {
+    const removeOrders = () => {
+      dispatch(closeDialog());
+      dispatch(deleteOrder(id));
     };
+    openSuccessSnackbar(removeOrders, REMOVE_ORDER_MESSAGE);
+  };
 
-    const orderItems =
-        ordersList &&
-        ordersList.map((order) => (
-            <TableContainerRow
-                key={order._id}
-                data={ReactHtmlParser(getTime(order.dateOfCreation, true))}
-                orderId={order.orderNumber}
-                customer={`${order?.user?.firstName} ${order?.user?.lastName}`}
-                totalPrice={`${order?.totalPriceToPay[0]?.value} ₴`}
-                paymentStatus={
-                    <Status
-                        status={orders.paymentStatusTranslation[order?.paymentStatus]}
-                    />
-                }
-                status={<Status status={orders.orderTableStatus[order.status]}/>}
-                editHandler={() => {
-                    dispatch(push(pathToOrderItem.replace(':id', order._id)));
-                }}
-                deleteHandler={() => {
-                    ordersDeleteHandler(order._id);
-                }}
-                showAvatar={false}
-            />
-        ));
-    if (orderLoading) {
-        return <LoadingBar/>
-    }
-    return (
-        <div className={commonStyles.container}>
-            <div className={`${commonStyles.adminHeader} ${styles.title}`}>
-                <Typography variant='h1' className={commonStyles.materialTitle}>
-                    {config.titles.orderTitles.mainPageTitle}
-                </Typography>
-                <div className={styles.filterBy}>
-                    <Button
-                        id='add-order'
-                        component={Link}
-                        to={pathToOrdersAddPage}
-                        variant='contained'
-                        color='primary'
-                        className={styles.addButton}
-                    >
-                        {ADD_ORDER}
-                    </Button>
-                </div>
-            </div>
-            <FilterNavbar
-                options={
-                    {
-                        sortOptions,
-                        filterByMultipleOptions,
-                        filterByDateOptions,
-                        clearOptions,
-                        searchOptions
-                    } || {}
-                }
-            />
-
-            <div className={commonStyles.table}>
-                {handleOrdersPage(
-                    ordersList,
-                    itemsCount,
-                    orderItems,
-                    commonStyles.noRecords
-                )}
-            </div>
+  const orderItems =
+    ordersList &&
+    ordersList.map((order) => (
+      <TableContainerRow
+        key={order._id}
+        data={ReactHtmlParser(getTime(order.dateOfCreation, true))}
+        orderId={order.orderNumber}
+        customer={`${order?.user?.firstName} ${order?.user?.lastName}`}
+        totalPrice={`${order?.totalPriceToPay[0]?.value} ₴`}
+        paymentStatus={
+          <Status
+            status={orders.paymentStatusTranslation[order?.paymentStatus]}
+          />
+        }
+        status={<Status status={orders.orderTableStatus[order.status]} />}
+        editHandler={() => {
+          dispatch(push(pathToOrderItem.replace(':id', order._id)));
+        }}
+        deleteHandler={() => {
+          ordersDeleteHandler(order._id);
+        }}
+        showAvatar={false}
+      />
+    ));
+  if (orderLoading) {
+    return <LoadingBar />;
+  }
+  return (
+    <div className={commonStyles.container}>
+      <div className={`${commonStyles.adminHeader} ${styles.title}`}>
+        <Typography variant='h1' className={commonStyles.materialTitle}>
+          {config.titles.orderTitles.mainPageTitle}
+        </Typography>
+        <div className={styles.filterBy}>
+          <Button
+            id='add-order'
+            component={Link}
+            to={pathToOrdersAddPage}
+            variant='contained'
+            color='primary'
+            className={styles.addButton}
+          >
+            {ADD_ORDER}
+          </Button>
         </div>
-    );
+      </div>
+      <FilterNavbar
+        options={
+          {
+            sortOptions,
+            filterByMultipleOptions,
+            filterByDateOptions,
+            clearOptions,
+            searchOptions
+          } || {}
+        }
+      />
+
+      <div className={commonStyles.table}>
+        {handleOrdersPage(
+          ordersList,
+          itemsCount,
+          orderItems,
+          commonStyles.noRecords
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrdersPage;
--- src/redux/orders/orders.reducer.js
+++ src/redux/orders/orders.reducer.js
@@ -1,144 +1,150 @@
 import {
-    SET_ORDER,
-    SET_ORDER_LOADING,
-    SET_ORDER_ERROR,
-    SET_ORDER_LIST,
-    REMOVE_ORDER_FROM_STORE,
-    SET_NOVAPOSHTA_CITIES,
-    SET_NOVAPOSHTA_WAREHOUSES,
-    SET_DELIVERY_LOADING,
-    SET_UKRPOST_REGIONS,
-    SET_UKRPOST_DISTRICTS,
-    SET_UKRPOST_CITIES,
-    SET_UKRPOST_POSTOFFICES,
-    CLEAR_FILTERS, SET_FILTER, SET_SORT, SET_ORDER_SORT_LABEL
+  SET_ORDER,
+  SET_ORDER_LOADING,
+  SET_ORDER_ERROR,
+  SET_ORDER_LIST,
+  REMOVE_ORDER_FROM_STORE,
+  SET_NOVAPOSHTA_CITIES,
+  SET_NOVAPOSHTA_WAREHOUSES,
+  SET_DELIVERY_LOADING,
+  SET_UKRPOST_REGIONS,
+  SET_UKRPOST_DISTRICTS,
+  SET_UKRPOST_CITIES,
+  SET_UKRPOST_POSTOFFICES,
+  CLEAR_FILTERS,
+  SET_FILTER,
+  SET_SORT,
+  SET_ORDER_SORT_LABEL
 } from './orders.types';
 
 const initialFilters = {
-    status: [],
-    paymentStatus: [],
-    dateFrom: '',
-    dateTo: '',
-    search: ''
+  status: [],
+  paymentStatus: [],
+  dateFrom: '',
+  dateTo: '',
+  search: ''
 };
 
 export const initialState = {
-    list: [],
-    filters: initialFilters,
-    sort: {},
-    selectedOrder: null,
-    orderLoading: false,
-    orderError: null,
-    deliveryLoading: false,
-    cities: [],
-    warehouses: [],
-    ukrPoshtaCities: [],
-    ukrPoshtaRegions: [],
-    ukrPoshtaDistricts: [],
-    ukrPoshtaPostOffices: [],
-    sortLabel:''
+  list: [],
+  filters: initialFilters,
+  sort: {
+    dateOfCreation: -1
+  },
+  selectedOrder: null,
+  orderLoading: false,
+  orderError: null,
+  deliveryLoading: false,
+  cities: [],
+  warehouses: [],
+  ukrPoshtaCities: [],
+  ukrPoshtaRegions: [],
+  ukrPoshtaDistricts: [],
+  ukrPoshtaPostOffices: [],
+  sortLabel: ''
 };
 
-export const selectOrderList = ({Orders}) => ({
-    orderLoading: Orders.orderLoading,
-    ordersList: Orders.list?.items
+export const selectOrderList = ({ Orders }) => ({
+  orderLoading: Orders.orderLoading,
+  ordersList: Orders.list?.items,
+  sort: Orders.sort
 });
 
 const ordersReducer = (state = initialState, action = {}) => {
-    switch (action.type) {
-        case SET_ORDER:
-            return {
-                ...state,
-                selectedOrder: action.payload
-            };
-        case SET_ORDER_SORT_LABEL:
-            return {
-                ...state,
-                sortLabel: action.payload
-            };
-        case SET_ORDER_LOADING:
-            return {
-                ...state,
-                orderLoading: action.payload
-            };
-        case SET_ORDER_ERROR:
-            return {
-                ...state,
-                orderError: action.payload
-            };
-        case SET_ORDER_LIST:
-            return {
-                ...state,
-                list: action.payload
-            };
-        case REMOVE_ORDER_FROM_STORE:
-            const orders = state.list.items.filter(
-                (order) => order._id !== action.payload
-            );
-            return {
-                ...state,
-                list: {...state.list, items: orders, count: state.list.count - 1}
-            };
-        case SET_UKRPOST_REGIONS:
-            return {
-                ...state,
-                ukrPoshtaRegions: action.payload
-            };
-        case SET_UKRPOST_DISTRICTS:
-            return {
-                ...state,
-                ukrPoshtaDistricts: action.payload
-            };
-        case SET_UKRPOST_CITIES:
-            return {
-                ...state,
-                ukrPoshtaCities: action.payload
-            };
-        case SET_UKRPOST_POSTOFFICES:
-            return {
-                ...state,
-                ukrPoshtaPostOffices: action.payload
-            };
-        case SET_NOVAPOSHTA_CITIES:
-            return {
-                ...state,
-                cities: action.payload
-            };
-        case SET_NOVAPOSHTA_WAREHOUSES:
-            return {
-                ...state,
-                warehouses: action.payload
-            };
-        case SET_DELIVERY_LOADING:
-            return {
-                ...state,
-                deliveryLoading: action.payload
-            };
-        case SET_FILTER:
-            return {
-                ...state,
-                filters: {
-                    ...state.filters,
-                    ...action.payload
-                }
-            };
-        case SET_SORT:
-            return {
-                ...state,
-                sort: {
-                    ...action.payload
-                }
-            };
-        case CLEAR_FILTERS:
-            return {
-                ...state,
-                filters: initialFilters,
-                sortLabel: "",
-                sort: {}
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+  case SET_ORDER:
+    return {
+      ...state,
+      selectedOrder: action.payload
+    };
+  case SET_ORDER_SORT_LABEL:
+    return {
+      ...state,
+      sortLabel: action.payload
+    };
+  case SET_ORDER_LOADING:
+    return {
+      ...state,
+      orderLoading: action.payload
+    };
+  case SET_ORDER_ERROR:
+    return {
+      ...state,
+      orderError: action.payload
+    };
+  case SET_ORDER_LIST:
+    return {
+      ...state,
+      list: action.payload
+    };
+  case REMOVE_ORDER_FROM_STORE:
+    const orders = state.list.items.filter(
+      (order) => order._id !== action.payload
+    );
+    return {
+      ...state,
+      list: { ...state.list, items: orders, count: state.list.count - 1 }
+    };
+  case SET_UKRPOST_REGIONS:
+    return {
+      ...state,
+      ukrPoshtaRegions: action.payload
+    };
+  case SET_UKRPOST_DISTRICTS:
+    return {
+      ...state,
+      ukrPoshtaDistricts: action.payload
+    };
+  case SET_UKRPOST_CITIES:
+    return {
+      ...state,
+      ukrPoshtaCities: action.payload
+    };
+  case SET_UKRPOST_POSTOFFICES:
+    return {
+      ...state,
+      ukrPoshtaPostOffices: action.payload
+    };
+  case SET_NOVAPOSHTA_CITIES:
+    return {
+      ...state,
+      cities: action.payload
+    };
+  case SET_NOVAPOSHTA_WAREHOUSES:
+    return {
+      ...state,
+      warehouses: action.payload
+    };
+  case SET_DELIVERY_LOADING:
+    return {
+      ...state,
+      deliveryLoading: action.payload
+    };
+  case SET_FILTER:
+    return {
+      ...state,
+      filters: {
+        ...state.filters,
+        ...action.payload
+      }
+    };
+  case SET_SORT:
+    return {
+      ...state,
+      sort: {
+        ...action.payload
+      }
+    };
+  case CLEAR_FILTERS:
+    return {
+      ...state,
+      filters: initialFilters,
+      sortLabel: '',
+      sort: {}
+    };
+  default:
+    return state;
+  }
 };
 
 export default ordersReducer;
