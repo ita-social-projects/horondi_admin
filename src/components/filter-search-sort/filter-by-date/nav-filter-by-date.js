4aa6575f41a66d93984ffcf6fa11873286194561
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-05-31 23:14:03 +0300

    added default

--- src/components/filter-search-sort/filter-by-date/nav-filter-by-date.js
+++ src/components/filter-search-sort/filter-by-date/nav-filter-by-date.js
@@ -1,78 +1,42 @@
-// import React from 'react';
-// import TextField from '@material-ui/core/TextField';
-// import PropTypes from 'prop-types';
-// import _ from 'lodash';
-//
-// import {useStyles} from './filter-by-date.styles';
-// import materialUiConstants from "../../../configs/material-ui-constants";
-// import {generateDateFormatForInputValue} from "../../../utils/history";
-//
-// const NavFilterByDate = ({filterByDateOptions: {filters, dateHandler, title}}) => {
-//     const styles = useStyles();
-//     const setDateHandler = ({target}) => {
-//         if (target.value) {
-//             dateHandler(new Date(target.value).getTime());
-//         }
-//     };
-//
-//     return (
-//         <form className={styles.container} noValidate>
-//             <TextField
-//                 id={materialUiConstants.types.datetimeLocal}
-//                 onBlur={setDateHandler}
-//                 label={title}
-//                 defaultValue={filters ? generateDateFormatForInputValue(filters) : ''}
-//                 type={materialUiConstants.types.datetimeLocal}
-//                 className={styles.textField}
-//                 InputLabelProps={{
-//                     shrink: true
-//                 }}
-//             />
-//         </form>
-//     );
-// };
-//
-// NavFilterByDate.propTypes = {
-//     filters: PropTypes.string,
-//     title: PropTypes.string,
-//     setDateRangeFilter: PropTypes.func
-// };
-//
-// NavFilterByDate.defaultProps = {
-//     filters: '',
-//     setDateRangeFilter: _.noop,
-//     title: ''
-// };
-//
-// export default NavFilterByDate;
-
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import { DateRangePicker } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
 
-const NavFilterByDate = (filterByDateOptions) => {
-  const [value, setValue] = useState([
-    new Date('2021-05-20'),
-    new Date('2021-05-29')
-  ]);
+const NavFilterByDate = ({
+  filterByDateOptions: { dateFrom, dateTo, dateHandler }
+}) => {
+  const { afterToday } = DateRangePicker;
+  const from = dateFrom || new Date();
+  const to = dateTo || new Date();
 
-  const setDateHandler = (v) => {
-    setValue(v);
-    if (filterByDateOptions) {
-      filterByDateOptions.filterByDateOptions[0].dateHandler(v[0].getTime());
-      filterByDateOptions.filterByDateOptions[1].dateHandler(v[1].getTime());
-    }
+  const [value, setValue] = useState([from, to]);
+  const setDateHandler = (value) => {
+    setValue(value);
+    dateHandler(value);
   };
-
   return (
     <DateRangePicker
       format='DD-MM-YYYY'
-      appearance='default'
-      placeholder='Default'
+      isoWeek
       style={{ width: 280 }}
       value={value}
-      onChange={(value) => setDateHandler(value)}
+      disabledDate={afterToday()}
+      onChange={setDateHandler}
     />
   );
 };
+
+NavFilterByDate.propTypes = {
+  dateFrom: PropTypes.string,
+  dateTo: PropTypes.string,
+  filterByDateOptions: PropTypes.objectOf(PropTypes.object)
+};
+NavFilterByDate.defaultProps = {
+  dateFrom: '',
+  dateTo: '',
+  filterByDateOptions: {}
+};
+
 export default NavFilterByDate;
