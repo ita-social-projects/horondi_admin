c79d085986b42d144836b5172094c14d46b9e76a
Martha Semeryak <martha.semeryak@gmail.com>
2021-06-22 21:47:31 +0300

    Added test for delete-button component

--- src/components/buttons/delete-button/delete-button.test.js
+++ src/components/buttons/delete-button/delete-button.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import DeleteButton from './delete-button';
 import { config } from '../../../configs';
@@ -12,8 +12,8 @@ describe('delete button tests', () => {
   const size = 'small';
   const id = '5f62f5386d3d7c14710c0111';
   const mockCallBack = jest.fn(() => id);
+  const text = DELETE_TITLE;
   let component;
-
   beforeEach(() => {
     component = mount(
       <DeleteButton size={size} onClickHandler={mockCallBack} />
@@ -22,13 +22,23 @@ describe('delete button tests', () => {
   afterEach(() => {
     component.unmount();
   });
-
   it('should exist', () => {
     expect(component).toMatchSnapshot();
     expect(component.exists());
   });
   it('should have type', () => {
     expect(component.find('button').type()).toEqual('button');
+    expect(component).toMatchSnapshot();
+  });
+  it('should click', () => {
+    expect(mockCallBack.mock.calls.length).toBe(0);
+    component.simulate('click');
+    expect(component.find('button'));
+  });
+  it('should exist and have value', () => {
+    expect(component.exists());
+    expect(component.prop('size')).toEqual(size);
+    expect(component.prop('onClickHandler')).toEqual(mockCallBack);
   });
   it('should have props', () => {
     expect(component.props().size).toBeDefined();
@@ -42,5 +52,25 @@ describe('delete button tests', () => {
   });
   it('should be one', () => {
     expect(component.find('button').length).not.toBe(2);
+  });
+  it('should renders children when passed in', () => {
+    const component = shallow(
+      <DeleteButton>
+        <div className='unique' />
+      </DeleteButton>
+    );
+    expect(component.contains(<div className='unique' />)).toEqual(true);
+  });
+  it('should have title text (children)', () => {
+    const component = shallow(<DeleteButton>{text}</DeleteButton>);
+    expect(component.text()).toBeDefined();
+    expect(component.text()).toEqual(DELETE_TITLE);
+    expect(component.text()).toBeTruthy();
+  });
+  it('should have className', () => {
+    const component = shallow(<DeleteButton />);
+    expect(component.hasClass('makeStyles-button-8')).toBeDefined();
+    expect(component.hasClass('makeStyles-button-8')).toBe(true);
+    expect(component).toMatchSnapshot();
   });
 });
