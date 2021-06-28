f58aa13b9d08900de22e001cdd7d8b0efbf32464
Ivan <romanomess@gmail.com>
2021-06-28 10:48:57 +0300

    fixed tests

--- src/pages/sizes/tests/mockStoreReturner.js
+++ src/pages/sizes/tests/mockStoreReturner.js
@@ -4,16 +4,18 @@ const mockStoreReturner = (mockLoading) => ({
       {
         _id: '604394cba7532c33dcb326d6',
         name: 'M',
-        simpleName: [
-          {
-            lang: 'ua',
-            value: 'Роллтоп'
-          },
-          {
-            lang: 'en',
-            value: 'Rolltop'
-          }
-        ],
+        modelId: {
+          name: [
+            {
+              lang: 'ua',
+              value: 'Роллтоп'
+            },
+            {
+              lang: 'en',
+              value: 'Rolltop'
+            }
+          ]
+        },
         heightInCm: 30,
         widthInCm: 27,
         depthInCm: 13,
