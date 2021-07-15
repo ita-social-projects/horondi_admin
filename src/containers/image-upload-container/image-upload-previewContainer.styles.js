8e92c72895fa9377c6f63f06963efeb7f1b5bd48
Ivan <romanomess@gmail.com>
2021-07-15 23:18:22 +0300

    added basic funcitonality for dropzone and styles for it respectively

--- src/containers/image-upload-container/image-upload-previewContainer.styles.js
+++ src/containers/image-upload-container/image-upload-previewContainer.styles.js
@@ -28,6 +28,28 @@ export const useStyles = makeStyles(() => ({
       cursor: 'pointer'
     }
   },
+  uploadContainer: {
+    display: 'inline-block',
+    marginLeft: 0,
+    cursor: 'pointer',
+    width: '200px',
+    height: '200px',
+    transition: '0.4s ease',
+    '&:hover': {
+      opacity: 0.5,
+      backgroundImage: `url(${upload})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    },
+    '& img': {
+      width: 'inherit',
+      height: 'inherit'
+    },
+    '&:hover > img': {
+      opacity: 0.1,
+      cursor: 'pointer'
+    }
+  },
   image: {
     width: '200px',
     height: '200px'
