d72ed4eada673c138b269f4360180a3dd6e262cb
christina_sokyrko <oni22de@gmail.com>
2021-07-28 23:53:59 +0300

    task/878

---
+++ src/components/forms/contacts-form/tests/contacts-form.test.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import * as reactRedux from 'react-redux';
+import ContactsForm from '../index';
+import { config } from '../../../../configs';
+import ImageUploadContainer from '../../../../containers/image-upload-container';
+import { contactSaveHandler, initialValues } from './contacts-form.variables';
+
+configure({ adapter: new Adapter() });
+
+const { GO_BACK_TITLE, SAVE_TITLE } = config.buttonTitles;
+
+const mockSetFieldValue = jest.fn();
+const mockSubmit = jest.fn();
+const mockChange = jest.fn();
+const mockBlur = jest.fn();
+
+jest.mock('formik', () => ({
+  ...jest.requireActual('formik'),
+  useFormik: () => ({
+    values: {},
+    handleSubmit: mockSubmit,
+    handleChange: mockChange,
+    touched: {
+      cartLink: 'test',
+      phoneNumber: '095769834',
+      email: '123@gmail.com'
+    },
+    errors: { cartLink: 'error', phoneNumber: 'wrong', email: 'incorrect' },
+    setFieldValue: mockSetFieldValue,
+    handleBlur: mockBlur
+  })
+}));
+
+const mockSetUpload = jest.fn();
+const mockSetContactsImage = jest.fn();
+
+jest.mock('../../../../utils/contacts-form', () => ({
+  __esModule: true,
+  setMapImageHandler: () => mockSetContactsImage(),
+  setInputsContactHandler: (schedule, address) => [
+    { label: schedule, name: 'schedule' },
+    { label: address, name: 'address' }
+  ]
+}));
+
+jest.spyOn(global, 'FileReader').mockImplementation(function () {
+  this.readAsDataURL = jest.fn();
+  this.onload = jest.fn();
+});
+
+describe('test СategoryForm', () => {
+  const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
+  let wrapper;
+
+  beforeEach(() => {
+    useDispatchMock.mockImplementation(() => jest.fn());
+    wrapper = mount(
+      <ContactsForm
+        contactSaveHandler={contactSaveHandler}
+        initialValues={initialValues}
+      />
+    );
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+    useDispatchMock.mockClear();
+  });
+
+  it('Should render ContactsForm component', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('Should render component form', () => {
+    expect(wrapper.find('form')).toHaveLength(1);
+  });
+
+  it(`Should render Go back button with '${GO_BACK_TITLE}' label`, () => {
+    expect(wrapper.find('button').at(0).text()).toBe(GO_BACK_TITLE);
+  });
+
+  it(`Should render Save button with '${SAVE_TITLE}' label`, () => {
+    expect(wrapper.find('button').at(1).text()).toBe(SAVE_TITLE);
+  });
+
+  it('Should handle uaSelectImageHandler', () => {
+    wrapper.find(ImageUploadContainer).at(0).props().handler();
+    expect(mockSetContactsImage).toHaveBeenCalled();
+  });
+  it('Should handle enSelectImageHandler', () => {
+    wrapper.find(ImageUploadContainer).at(1).props().handler();
+    expect(mockSetContactsImage).toHaveBeenCalledTimes(2);
+  });
+
+  // it('Should render two  buttons and two inputs', () => {
+  //   expect(wrapper.find('input')).toHaveLength(2);
+  //   expect(wrapper.find('button')).toHaveLength(2);
+  // });
+
+  // it('Should simulate change event', () => {
+  //   wrapper.find('input').at(1).simulate('change');
+  //   expect(mockChange).toHaveBeenCalledTimes(1);
+  // });
+
+  // it('Should simulate blur event', () => {
+  //   wrapper.find('input').at(1).simulate('blur');
+  //   expect(mockBlur.mock.calls.length).toBe(1);
+  // });
+
+  // it('Should simulate form submit', () => {
+  //   wrapper.find('form').simulate('submit');
+  //   expect(mockSubmit).toHaveBeenCalledTimes(0);
+  // });
+
+  // it('Should simulate submit button', () => {
+  //   wrapper.find('button').at(1).simulate('click');
+  //   expect(mockChange.mock.calls.length).toEqual(1);
+  // });
+
+  // it('Should simulate back button', () => {
+  //   wrapper.find('button').at(0).simulate('click');
+  //   expect(mockChange).toHaveBeenCalledTimes(1);
+  // });
+
+  // it('should render ImageUploadContainer component', () => {
+  //   const wrap = wrapper.find(ImageUploadContainer);
+  //   expect(wrap.exists(ImageUploadContainer)).toBeDefined();
+  //   expect(wrap.exists(ImageUploadContainer)).toBe(true);
+  // });
+
+  // it('Should upload image', () => {
+  //   const imageContainer = wrapper.find(ImageUploadContainer);
+  //   const handler = imageContainer.prop('handler');
+  //   handler(files);
+  //   expect(mockSetUpload).toHaveBeenCalledTimes(1);
+  //   expect(mockSetUpload).toHaveBeenCalledWith(files[0]);
+  // });
+
+  // it('Should test FileReader ', () => {
+  //   const reader = FileReader.mock.instances[0];
+  //   reader.onload(target);
+  //   expect(reader.readAsDataURL).toHaveBeenCalled();
+  //   expect(reader.readAsDataURL).toHaveBeenCalledWith(files[0]);
+  // });
+
+  // it('Should test CategoryImage', () => {
+  //   const reader = FileReader.mock.instances[0];
+  //   reader.onload(target);
+  //   expect(mockSetCategoryImage).toHaveBeenCalled();
+  //   expect(mockSetCategoryImage).toHaveBeenCalledWith('foo');
+  // });
+
+  // it('Should test FieldValue', () => {
+  //   const reader = FileReader.mock.instances[0];
+  //   reader.onload(target);
+  //   expect(mockSetFieldValue).toHaveBeenCalled();
+  //   expect(mockSetFieldValue).toHaveBeenCalledWith('categoryImage', 'foo');
+  // });
+
+  // it('Should have appropriate prop types', () => {
+  //   expect(CategoryForm.propTypes.id).toBe(PropTypes.string);
+  //   expect(CategoryForm.propTypes.edit).toBe(PropTypes.bool);
+  // });
+
+  // it('Should have default props', () => {
+  //   expect(CategoryForm.defaultProps).toBeDefined();
+  //   expect(CategoryForm.defaultProps.id).toBe('');
+  //   expect(CategoryForm.defaultProps.values).toEqual({});
+  //   expect(CategoryForm.defaultProps.errors).toEqual({});
+  //   expect(CategoryForm.defaultProps.touched).toEqual({});
+  //   expect(CategoryForm.defaultProps.category._id).toBe('');
+  //   expect(CategoryForm.defaultProps.edit).toBeFalsy();
+  //   expect(CategoryForm.defaultProps.category.code).toBe('');
+  // });
+});
---
+++ src/components/forms/contacts-form/tests/contacts-form.variables.js
@@ -0,0 +1,13 @@
+const contactSaveHandler = () => {};
+const initialValues = {
+  phoneNumber: '0662837452',
+  uaSchedule: 'тест',
+  enSchedule: 'test',
+  uaAddress: 'адреса',
+  enAddress: 'adress',
+  uaCartImage: 'зображення',
+  enCartImage: 'image',
+  email: 'something@gmail.com',
+  cartLink: 'www.google.com'
+};
+module.exports = { contactSaveHandler, initialValues };
