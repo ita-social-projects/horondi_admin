70e73249e91b5ab454b460d17f278a378750520e
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-08-19 22:49:41 +0300

    created test for pocker form

---
+++ src/components/forms/pockets-form/tests/mockPockets.js
@@ -0,0 +1,54 @@
+const target = { target: { result: 'foo' } };
+
+const files = [new File([], 'foo,png', { type: 'image' })];
+
+const mockPosition = {
+  positionsList: {},
+  loadingPositions: false
+};
+
+const Sizes = {
+  list: [
+    {
+      _id: '604394a2a7532c33dcb326d5',
+      name: 'L',
+      modelId: {
+        _id: '6043bf9e3e06ad3edcdb7b30',
+        name: [
+          {
+            value: 'Роллтоп',
+            lang: 'ua'
+          },
+          {
+            value: 'Rolltop',
+            lang: 'en'
+          }
+        ]
+      },
+      available: false
+    }
+  ]
+};
+const pocket = {
+  id: '',
+  values: {},
+  errors: {},
+  touched: {},
+  pocket: {
+    _id: '',
+    name: [[Object], [Object]],
+    images: { thumbnail: '' },
+    restrictions: false,
+    optionType: null,
+    additionalPrice: [[Object], [Object]],
+    positions: []
+  },
+  edit: false
+};
+
+module.exports = {
+  Sizes,
+  mockPosition,
+  target,
+  files
+};
---
+++ src/components/forms/pockets-form/tests/pockets-form.test.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import * as reactRedux from 'react-redux';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { act, fireEvent, render } from '@testing-library/react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import PocketsForm from '../index';
+import ImageUploadContainer from '../../../../containers/image-upload-container';
+
+import { files, target, mockPosition, Sizes } from './mockPockets';
+
+configure({ adapter: new Adapter() });
+
+const mockSetFieldValue = jest.fn();
+const mockSubmit = jest.fn();
+const mockChange = jest.fn();
+const mockBlur = jest.fn();
+const mockSetUpload = jest.fn();
+const mockSetPocketsImage = jest.fn();
+const getOptionSelected = jest.fn();
+
+jest.mock('formik', () => ({
+  ...jest.requireActual('formik'),
+  useFormik: () => ({
+    values: {},
+    handleSubmit: mockSubmit,
+    handleChange: mockChange,
+    touched: {},
+    errors: {},
+    setFieldValue: mockSetFieldValue,
+    handleBlur: mockBlur
+  })
+}));
+
+jest.mock('../../../../utils/use-pockets-handlers', () => ({
+  __esModule: true,
+  default: () => ({
+    setUpload: mockSetUpload,
+    setPocketsImage: mockSetPocketsImage
+  })
+}));
+
+jest.spyOn(global, 'FileReader').mockImplementation(function () {
+  this.readAsDataURL = jest.fn();
+  this.onload = jest.fn();
+});
+
+describe('pocket form tests', () => {
+  let spyOnUseSelector;
+  let spyOnUseDispatch;
+  let component;
+
+  beforeEach(() => {
+    spyOnUseSelector = jest.spyOn(reactRedux, 'useSelector');
+    spyOnUseSelector.mockImplementation(() => mockPosition);
+    spyOnUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
+    spyOnUseDispatch.mockImplementation(() => jest.fn());
+
+    component = mount(<PocketsForm />);
+  });
+  afterEach(() => {
+    component.unmount();
+    spyOnUseDispatch.mockClear();
+    spyOnUseSelector.mockClear();
+  });
+
+  it('Should upload image', () => {
+    const imageContainer = component.find(ImageUploadContainer);
+    const handler = imageContainer.prop('handler');
+    handler(files);
+    expect(mockSetUpload).toHaveBeenCalledTimes(1);
+    expect(mockSetUpload).toHaveBeenCalledWith(files[0]);
+  });
+
+  it('Should test FileReader ', () => {
+    const reader = FileReader.mock.instances[0];
+    reader.onload(target);
+    expect(reader.readAsDataURL).toHaveBeenCalled();
+    expect(reader.readAsDataURL).toHaveBeenCalledWith(files[0]);
+  });
+
+  it('Should update checkboxes checked value on click', () => {
+    const { getByRole } = render(<PocketsForm />);
+    act(() => {
+      fireEvent.click(getByRole('checkbox'));
+    });
+    expect(mockSetFieldValue).toHaveBeenCalled();
+  });
+
+  it('Should click onsubmit button', () => {
+    const event = {
+      preventDefault: () => {}
+    };
+    jest.spyOn(event, 'preventDefault');
+    component.find('form').simulate('submit', event);
+    expect(event.preventDefault).toBeCalled();
+  });
+
+  it('should coverage getOptionSelected in AutoComplete', () => {
+    const label = component.find(Autocomplete).props();
+    label.getOptionSelected(
+      '604394a2a7532c33dcb326d5',
+      '604394a2a7532c33dcb326d5'
+    );
+    expect(label).toBeDefined();
+  });
+
+  it('should coverage getOptionLabel in Autocomplete', () => {
+    const label = component.find(Autocomplete).props();
+    label.getOptionLabel(Sizes.list[0]);
+    expect(label).toBeDefined();
+  });
+
+  it('should coverage onTagsChange', () => {
+    // const label = component.find(Autocomplete).props();
+    // label.getOptionLabel(Sizes.list[0]);
+    // expect(label).toBeDefined();
+    // const test = component.find('checkIsEdit')
+    // console.log(test.debug())
+  });
+
+  // it('Should click onsubmit button', () => {
+  //     const event = { preventDefault: () => {} };
+  //     jest.spyOn(event, 'preventDefault');
+  //     component.find('form').simulate('submit', event);
+  //     expect(event.preventDefault).toBeCalled();
+  // });
+});
