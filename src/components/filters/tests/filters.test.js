32ba701a70ec3a89f544a08c4d71f9ff272808c2
Skrekliam <typnickman@gmail.com>
2021-09-17 22:17:34 +0300

    Increased test coverage for filters

--- src/components/filters/tests/filters.test.js
+++ src/components/filters/tests/filters.test.js
@@ -1,36 +1,153 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import * as reactRedux from 'react-redux';
 import { act } from 'react-dom/test-utils';
 import IconButton from '@material-ui/core/IconButton';
+import { DateRangePicker as DateRangeSelector } from 'rsuite';
+import InputBase from '@material-ui/core/InputBase';
+import { Select } from '@material-ui/core';
 
-import Clear from '../clear/clear';
+import { wrap } from 'lodash';
+import Clear from '../clear';
+import DateRangePicker from '../date-range-picker';
+import OptionPicker from '../option-picker';
+import OptionsPicker from '../options-picker';
+import Search from '../search';
+import { config } from '../../../configs';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const { submitKey } = config;
+
 let wrapper;
-let spyOnUseDispatch;
-let mockDispatch;
-let spyOnUseSelector;
+let props;
+let handler;
 
 describe('Testing filters', () => {
   beforeEach(() => {
-    spyOnUseSelector = jest.spyOn(reactRedux, 'useSelector');
-    spyOnUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
-
-    mockDispatch = jest.fn();
-
-    spyOnUseDispatch.mockReturnValue(mockDispatch);
+    handler = jest.fn();
   });
 
   afterEach(() => {
     wrapper = null;
+    handler = null;
   });
 
-  it('Should render "Clear"', () => {
-    wrapper = mount(<Clear />);
+  describe('Clear filter', () => {
+    it('Should render', () => {
+      wrapper = mount(<Clear />);
 
-    expect(wrapper).toBeDefined();
+      expect(wrapper).toBeDefined();
+    });
+  });
+
+  describe('DateRangePicker filter', () => {
+    it('Should render with date', () => {
+      props = { dateFrom: new Date(), dateTo: new Date() };
+
+      wrapper = mount(<DateRangePicker {...props} />);
+      expect(wrapper).toBeDefined();
+    });
+
+    it('Should render without date', () => {
+      props = { dateFrom: undefined, dateTo: undefined };
+
+      wrapper = mount(<DateRangePicker {...props} />);
+
+      expect(wrapper).toBeDefined();
+    });
+
+    it('Should select range', () => {
+      props = { dateFrom: new Date(), dateTo: new Date() };
+
+      wrapper = mount(<DateRangePicker {...props} />);
+      console.log(wrapper.find(DateRangePicker).last().props());
+    });
+  });
+
+  describe('OptionPicker filter', () => {
+    it('Should render', () => {
+      props = { options: ['test'] };
+
+      wrapper = mount(<OptionPicker {...props} />);
+
+      expect(wrapper).toBeDefined();
+    });
+
+    it('Should call setOptionHandler', () => {
+      props = { options: [{ value: 'test' }], handler };
+
+      wrapper = mount(<OptionPicker {...props} />);
+      wrapper
+        .find(Select)
+        .props()
+        .onChange({ target: { value: 'test' } });
+      wrapper
+        .find(Select)
+        .props()
+        .onChange({ target: { value: 'not test' } });
+
+      expect(handler.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('OptionsPicker filter', () => {
+    it('Should render', () => {
+      props = { options: ['test'], value: ['test'] };
+
+      wrapper = mount(<OptionsPicker {...props} />);
+      expect(wrapper).toBeDefined();
+    });
+
+    it('Should call setOptionsHandler', () => {
+      const value = ['test'];
+      const label = 'test';
+      props = { options: [{ value, label }], handler, value };
+
+      wrapper = mount(<OptionsPicker {...props} />);
+      wrapper
+        .find(Select)
+        .props()
+        .onChange({ target: { value: 'test' } });
+      wrapper
+        .find(Select)
+        .props()
+        .onChange({ target: { value: undefined } });
+
+      expect(handler.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('Search filter', () => {
+    it('Should render', () => {
+      wrapper = mount(<Search />);
+
+      expect(wrapper).toBeDefined();
+    });
+
+    it('Should submit on keyPress', () => {
+      props = { handler };
+      wrapper = mount(<Search {...props} />);
+      act(() => {
+        wrapper.find(InputBase).props().onKeyPress({ key: submitKey });
+        wrapper.find(InputBase).props().onKeyPress({ key: null });
+      });
+      expect(handler.mock.calls.length).toBe(1);
+    });
+
+    it('Should handle search on icon click ', () => {
+      props = { handler };
+
+      wrapper = mount(<Search {...props} />);
+      // act(() => {
+      //   wrapper
+      //     .find(InputBase)
+      //     .props()
+      //     .onChange({ target: { value: 'test' } });
+      // });
+      wrapper.find(IconButton).props().onClick();
+
+      expect(handler).toHaveBeenCalled();
+    });
   });
 });
