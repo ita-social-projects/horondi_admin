413697a50046324efb217b5130df429a5d44366a
Skrekliam <typnickman@gmail.com>
2021-09-23 23:33:45 +0300

    clear comments

--- src/components/filters-components/tests/filters-components.test.js
+++ src/components/filters-components/tests/filters-components.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { act } from 'react-dom/test-utils';
-import IconButton from '@material-ui/core/IconButton';
-import InputBase from '@material-ui/core/InputBase';
-import * as reactRedux from 'react-redux';
 
-import { Select } from '@material-ui/core';
-import { DateRangePicker as DateRangeSelector } from 'rsuite';
+import * as reactRedux from 'react-redux';
 
 import ComponentFilterClear from '../filter-clear/component-filter-clear';
 import ComponentFilterSinglePicker from '../filter-single-picker';
@@ -19,15 +14,8 @@ import { paymentStatusFilterObj } from '../../../utils/order';
 import ComponentFilterSearch from '../filter-search';
 import ComponentFilterRadioPicker from '../filter-radio-picker/component-filter-radio-picker';
 import ComponentFilterDateRangePicker from '../filter-date-range-picker';
-// import DateRangePicker from '../date-range-picker';
-// import OptionPicker from '../option-picker';
-// import OptionsPicker from '../options-picker';
-// import Search from '../search';
-// import { config } from '../../../configs';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-// const { submitKey } = config;
 
 let wrapper;
 let spyOnUseDispatch;
@@ -160,112 +148,4 @@ describe('Testing filters components', () => {
       expect(wrapper).toBeDefined();
     });
   });
-
-  // xdescribe('DateRangePicker filter', () => {
-  //   it('Should render with date', () => {
-  //     props = { dateFrom: new Date(), dateTo: new Date() };
-
-  //     wrapper = mount(<DateRangePicker {...props} />);
-  //     expect(wrapper).toBeDefined();
-  //   });
-
-  //   it('Should render without date', () => {
-  //     props = { dateFrom: undefined, dateTo: undefined };
-
-  //     wrapper = mount(<DateRangePicker {...props} />);
-
-  //     expect(wrapper).toBeDefined();
-  //   });
-
-  //   it('Should call setDateHander', () => {
-  //     const value = ['test'];
-  //     const label = 'test';
-  //     props = { options: [{ value, label }], handler, value };
-  //     wrapper = mount(<DateRangePicker {...props} />);
-  //     wrapper.find(DateRangeSelector).props().onChange('test');
-
-  //     expect(handler.mock.calls.length).toBe(1);
-  //   });
-  // });
-
-  // xdescribe('OptionPicker filter', () => {
-  //   it('Should render', () => {
-  //     props = { options: ['test'] };
-
-  //     wrapper = mount(<OptionPicker {...props} />);
-
-  //     expect(wrapper).toBeDefined();
-  //   });
-
-  //   it('Should call setOptionHandler', () => {
-  //     props = { options: [{ value: 'test' }], handler };
-
-  //     wrapper = mount(<OptionPicker {...props} />);
-  //     wrapper
-  //       .find(Select)
-  //       .props()
-  //       .onChange({ target: { value: 'test' } });
-  //     wrapper
-  //       .find(Select)
-  //       .props()
-  //       .onChange({ target: { value: 'not test' } });
-
-  //     expect(handler.mock.calls.length).toBe(1);
-  //   });
-  // });
-
-  // xdescribe('OptionsPicker filter', () => {
-  //   beforeEach(() => {
-  //     const value = ['test'];
-  //     const label = 'test';
-  //     props = { options: [{ value, label }], handler, value };
-  //   });
-
-  //   it('Should render', () => {
-  //     wrapper = mount(<OptionsPicker {...props} />);
-
-  //     expect(wrapper).toBeDefined();
-  //   });
-
-  //   it('Should call setOptionsHandler', () => {
-  //     wrapper = mount(<OptionsPicker {...props} />);
-  //     wrapper
-  //       .find(Select)
-  //       .props()
-  //       .onChange({ target: { value: 'test' } });
-  //     wrapper
-  //       .find(Select)
-  //       .props()
-  //       .onChange({ target: { value: undefined } });
-
-  //     expect(handler.mock.calls.length).toBe(1);
-  //   });
-  // });
-
-  // xdescribe('Search filter', () => {
-  //   it('Should render', () => {
-  //     wrapper = mount(<Search />);
-
-  //     expect(wrapper).toBeDefined();
-  //   });
-
-  //   it('Should submit on keyPress', () => {
-  //     props = { handler };
-  //     wrapper = mount(<Search {...props} />);
-  //     act(() => {
-  //       wrapper.find(InputBase).props().onKeyPress({ key: submitKey });
-  //       wrapper.find(InputBase).props().onKeyPress({ key: null });
-  //     });
-  //     expect(handler.mock.calls.length).toBe(1);
-  //   });
-
-  //   it('Should handle search on icon click ', () => {
-  //     props = { handler };
-
-  //     wrapper = mount(<Search {...props} />);
-  //     wrapper.find(IconButton).props().onClick();
-
-  //     expect(handler).toHaveBeenCalled();
-  //   });
-  // });
 });
