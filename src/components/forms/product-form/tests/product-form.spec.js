cf239322d941cc66ba6175c3dbf0c95e84d016f3
Bohdan Shchur <bodjashchur@gmail.com>
2021-10-21 00:58:11 +0300

    refresh tests

--- src/components/forms/product-form/tests/product-form.spec.js
+++ src/components/forms/product-form/tests/product-form.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as reactRedux from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@material-ui/core';
 import ProductForm from '../product-form';
 import { Products } from './product-form.variables';
@@ -21,34 +21,33 @@ jest.mock('formik', () => ({
   })
 }));
 
+jest.mock('react-redux');
+jest.mock('connected-react-router', () => ({
+  __esModule: true,
+  push: jest.fn()
+}));
+
+const mockDispatch = jest.fn();
+
+useDispatch.mockImplementation(mockDispatch);
+useSelector.mockImplementation((fn) => fn({ Products }));
+
 describe('Product-form tests', () => {
   let component;
-  let spyOnUseSelector;
-  let spyOnUseDispatch;
-  const Theme = {
-    darkMode: true
-  };
+
   beforeEach(() => {
-    spyOnUseSelector = jest.spyOn(reactRedux, 'useSelector');
-    spyOnUseSelector.mockImplementation((fn) => fn({ Products, Theme }));
-
-    spyOnUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
-    spyOnUseDispatch.mockImplementation(() => jest.fn());
-
     component = shallow(<ProductForm isEdit />);
   });
 
   afterEach(() => {
     component.unmount();
-    spyOnUseDispatch.mockClear();
-    spyOnUseDispatch.mockClear();
   });
 
   it('#1 Render the component', () => {
     expect(component.exists(ProductAddImages)).toBe(true);
   });
 
-  it('#2 Render the component', () => {
+  it('#2 Find checkboxes options', () => {
     const checkboxes = component.find(CheckboxOptions);
     checkboxes.props().options[0].handler();
     checkboxes.props().options[1].handler();
@@ -58,6 +57,6 @@ describe('Product-form tests', () => {
   it('#3 should submit the form', async () => {
     const submit = component.find(Button);
     submit.simulate('click');
-    expect(await spyOnUseDispatch).toHaveBeenCalledTimes(8);
+    expect(await mockDispatch).toHaveBeenCalledTimes(8);
   });
 });
