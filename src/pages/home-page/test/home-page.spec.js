731c3ba813c62fe0a437e950ec7617f71bd3a21a
marian muzyka <mrjnmzk@gmail.com>
2022-01-11 21:01:44 +0200

    test

--- src/pages/home-page/test/home-page.spec.js
+++ src/pages/home-page/test/home-page.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Avatar, Typography, Grid, Paper } from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 import HomePage from '../home-page';
+import LoadingBar from '../../../components/loading-bar';
+import selectPhotosAndLoading from '../../../redux/selectors/homepage.selectors';
 
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => ({
@@ -9,12 +12,12 @@ jest.mock('react-redux', () => ({
   useDispatch: jest.fn()
 }));
 
-const data = {
+const userData = {
   loading: HomePage.homePageLoading,
   photos: HomePage.photos
 };
 
-useSelector.mockImplementation(() => data);
+useSelector.mockImplementation(() => userData);
 useDispatch.mockImplementation(() => mockDispatch);
 
 describe('test HomePage component', () => {
@@ -25,4 +28,46 @@ describe('test HomePage component', () => {
     });
     expect(h1).toBeInTheDocument();
   });
+  // it('should render table component', () => {
+  //   render(<Grid />);
+  //   const header = screen.getByRole('document', { name: <body /> });
+  //   screen.debug(header);
+
+  //   // const div = getByRole('Grid');
+  //   // userEvent.type(screen.getByLabelText(/email/i), 'john.dee@someemail.com');
+  //   // fireEvent.click(screen.getByTestId('generate'));
+  //   // expect(screen.getByRole('grid')).toBeInTheDocument();
+  //   // expect(screen.getAllByRole('columnheader')).toHaveLength(3);
+
+  //   // expect(div).toBeInTheDocument();
+  // });
+  it('should render Grid component', () => {
+    const { getByRole } = render(<Grid />);
+    const wrapper = getByRole('input');
+    screen.debug(wrapper);
+    expect(wrapper).toBeInTheDocument();
+  });
+
+  // it('should render Paper component', () => {
+  //   const { getByClass } = render(<Paper />);
+  //   const wrapper = screen.getByClass(
+  //     'MuiPaper-root MuiPaper-elevation1 MuiPaper-rounded'
+  //   );
+  //   expect(wrapper).toBeInTheDocument();
+  // });
+
+  // const createRender = () => {
+  //   return render(<Grid />);
+  // };
+
+  // it('should render col', () => {
+  //   // const { getByText } = render(<Grid />);
+  //   const { getByText } = createRender();
+  //   expect(getByText('col')).toBeInTheDocument();
+  // });
+  // it('should render row', () => {
+  //   // const { getByText } = render(<Grid />);
+  //   const { getByText } = createRender();
+  //   expect(getByText('row')).toBeInTheDocument();
+  // });
 });
