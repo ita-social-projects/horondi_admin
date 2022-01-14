00cd09e0365ed788bae3a735c6da81a787238000
marian muzyka <mrjnmzk@gmail.com>
2022-01-14 13:01:51 +0200

    smallRefTest

--- src/pages/home-page/test/home-page.spec.js
+++ src/pages/home-page/test/home-page.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { useSelector, useDispatch } from 'react-redux';
 import HomePage from '../home-page';
 import LoadingBar from '../../../components/loading-bar';
@@ -31,29 +31,40 @@ const userData = {
 useSelector.mockImplementation(() => userData);
 useDispatch.mockImplementation(() => mockDispatch);
 
-describe('test HomePage component', () => {
+describe('test HomePage component, renders correctly', () => {
+  beforeEach(() => {
+    const { getByText, getAllByRole } = render(<HomePage />);
+  });
+
   it('should render h1 label', () => {
-    const { getByText } = render(<HomePage />);
-    const h1 = getByText(/Головна сторінка/i, {
+    const h1 = screen.getByText(/Головна сторінка/i, {
       selector: 'h1'
     });
+
     expect(h1).toBeInTheDocument();
   });
+
   it('should render 2 imgages', () => {
-    const { getAllByRole } = render(<HomePage />);
-    const images = getAllByRole('img');
+    const images = screen.getAllByRole('img');
+
     expect(images.length).toBe(2);
   });
-  it('Clicking on the input calls the `photoUpdateHandler` function', () => {
+
+  it('clicking on the input calls the `photoUpdateHandler` function', () => {
     const mockPhotoUpdateHandler = jest.fn();
     const wrapper = shallow(<HomePage />);
     const input = wrapper.find('input').at(0);
     input.simulate('click', { mockPhotoUpdateHandler: jest.fn() });
+
     expect(mockPhotoUpdateHandler).toBeTruthy();
   });
+});
+
+describe('test HomePage component, loading turned on', () => {
   it('should render Loading component', () => {
     userData.loading = true;
     render(<HomePage />);
+
     expect(<LoadingBar />).toBeTruthy();
   });
 });
