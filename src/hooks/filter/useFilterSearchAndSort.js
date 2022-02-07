508b1d6975b32394966a3e3c36bf41479ea75697
Kitsak Andrii <astrmgorlendort@gmail.com>
2022-02-07 09:27:00 +0200

    fix some code smell

--- src/hooks/filter/useFilterSearchAndSort.js
+++ src/hooks/filter/useFilterSearchAndSort.js
@@ -1,9 +1,12 @@
 import { useCallback } from 'react';
 
-export const useSort = (options, handler) => useCallback((e) => {
+export const useSort = (options, handler) => {
+  const setOptionHandler = useCallback((e) => {
     const result = options.find((item) => item.value === e.target.value);
 
     if (result) {
       handler(result);
     }
   });
+  return setOptionHandler;
+};
