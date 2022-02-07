a0d4c3a1f12a8e58961fb1270f0d8ebfee443c5a
Filiczini <51033321+Filiczini@users.noreply.github.com>
2022-02-07 15:38:27 +0200

    remove appolo mock

--- src/pages/about-us/tests/about-us.spec.js
+++ src/pages/about-us/tests/about-us.spec.js
@@ -2,29 +2,14 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import { MockedProvider } from '@apollo/client/testing';
->>>>>>> 9008768b (fix git comments)
 import { Button } from '@material-ui/core';
-=======
->>>>>>> c2197c98 (draft)
-=======
-import { Button } from '@material-ui/core';
->>>>>>> c797db05 (fix container row default props)
 import AboutUs from '../about-us';
 import { theme } from '../../../components/app/app-theme/app.theme';
 import TableContainerRow from '../../../containers/table-container-row';
 import TableContainerGenerator from '../../../containers/table-container-generator';
 
 jest.mock('react-redux');
-<<<<<<< HEAD
-
-=======
-jest.mock('@apollo/client');
->>>>>>> c2197c98 (draft)
 jest.mock('../about-us.styles', () => ({
   useStyles: () => ({})
 }));
@@ -49,44 +34,16 @@ describe('AboutUs component tests, ', () => {
       </MockedProvider>
     );
   });
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> c2197c98 (draft)
-=======
-
->>>>>>> 138f7d37 (resolve git comments)
   afterEach(() => {
     jest.restoreAllMocks();
     wrapper = null;
   });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  it('Component TableContainerRow should exist', () => {
-    expect(wrapper.exists(TableContainerRow)).toBe(true);
-  });
-
-  it('Component button should exist', () => {
-    expect(wrapper.exists(Button)).toBe(true);
-  });
-
-=======
   it('Should render AboutUs', () => {
     expect(wrapper).toBeDefined();
   });
-  it('Component TableContainerRow should exist', () => {
-    expect(wrapper.exists(TableContainerRow)).toBe(true);
-  });
-<<<<<<< HEAD
->>>>>>> c2197c98 (draft)
-=======
-  it('Component button should exist', () => {
-    expect(wrapper.exists(Button)).toBe(true);
-  });
->>>>>>> c797db05 (fix container row default props)
-=======
+
   it('Component TableContainerRow should exist', () => {
     expect(wrapper.exists(TableContainerRow)).toBe(true);
   });
@@ -95,7 +52,6 @@ describe('AboutUs component tests, ', () => {
     expect(wrapper.exists(Button)).toBe(true);
   });
 
->>>>>>> 138f7d37 (resolve git comments)
   it('Component TableContainerGenerator should exist', () => {
     expect(wrapper.exists(TableContainerGenerator)).toBe(true);
   });
