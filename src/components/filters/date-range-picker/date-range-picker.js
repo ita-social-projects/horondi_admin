a1f6a53e7df49d93ce19c01cee71b321a450e7b4
Kitsak Andrii <astrmgorlendort@gmail.com>
2022-02-10 16:30:46 +0200

    resolve code smell

--- src/components/filters/date-range-picker/date-range-picker.js
+++ src/components/filters/date-range-picker/date-range-picker.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import { DateRangePicker as DateRangeSelector } from 'rsuite';
