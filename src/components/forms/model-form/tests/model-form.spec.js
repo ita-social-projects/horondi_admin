b15ec9b8a0de04edc7b90f145aa8573dc1cb0122
Ihor Kytsak <kitsak735@gmail.com>
2022-03-30 00:02:19 +0300

    tests fix

--- src/components/forms/model-form/tests/model-form.spec.js
+++ src/components/forms/model-form/tests/model-form.spec.js
@@ -94,8 +94,8 @@ describe('Model-form tests', () => {
     expect(wrapper.exists('form')).toBe(true);
   });
 
-  it(' should call useEffect four times', () => {
-    expect(mockUseEffect).toHaveBeenCalledTimes(4);
+  it(' should call useEffect two times', () => {
+    expect(mockUseEffect).toHaveBeenCalledTimes(2);
   });
 
   it(' should call useSelector twice', () => {
