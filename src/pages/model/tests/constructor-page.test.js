c62e558cb647de0f39911ac04a6860304fd15fdc
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-04-27 09:46:54 +0300

    tests

--- src/pages/model/tests/constructor-page.test.js
+++ src/pages/model/tests/constructor-page.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -9,7 +8,6 @@ import { store, match } from './constructor-page.variables';
 import { config } from '../../../configs';
 
 const { constructorBasic } = config.labels.model;
-const { DELETE_TITLE } = config.buttonTitles;
 
 jest.mock('../constructor/constructor-page/constructor-page.styles.js', () => ({
   useStyles: () => ({})
@@ -38,10 +36,5 @@ describe('Testing ConstructorPage', () => {
   it(`Should render component with tab named as '${constructorBasic}' set as default tab`, () => {
     const firstTab = screen.getAllByText(constructorBasic)[0].closest('button');
     expect(firstTab.getAttribute('aria-selected')).toBe('true');
-  });
-
-  it('', async () => {
-    const deleteBtn = screen.getAllByTitle(DELETE_TITLE)[0];
-    userEvent.click(deleteBtn);
   });
 });
