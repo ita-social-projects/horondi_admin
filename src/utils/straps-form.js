563dbdcc0d33ee30b7cdfa8390f44fa4f0ddcba2
Evhenij Pipa <zhuvchuk1@gmail.com>
2022-06-19 15:01:00 +0300

    added relativePrice to straps

--- src/utils/straps-form.js
+++ src/utils/straps-form.js
@@ -3,7 +3,9 @@ export const getStrapsInitialValues = (IMG_URL, strap) => ({
   uaName: strap.name[0].value || '',
   enName: strap.name[1].value || '',
   available: strap.available,
-  additionalPrice: strap?.absolutePrice || '',
-  additionalPriceType: 'ABSOLUTE',
+  additionalPriceType: strap.absolutePrice ? 'ABSOLUTE' : 'RELATIVE',
+  additionalPrice: strap.absolutePrice
+    ? strap.absolutePrice
+    : strap.relativePrice,
   color: strap.features.color._id
 });
