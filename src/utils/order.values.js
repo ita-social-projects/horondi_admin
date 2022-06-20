99532699c5170050a26613b1784826fcd6a1ccf8
Anatoliy Gargash <kabantolyan@gmail.com>
2022-06-20 18:26:46 +0300

    fix duplication 2

--- src/utils/order.values.js
+++ src/utils/order.values.js
@@ -2,49 +2,59 @@ import config from '../configs/orders';
 
 const { deliveryTypes } = config;
 
+const recipient = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phoneNumber: ''
+};
+
+const courier = {
+  region: '',
+  district: '',
+  city: '',
+  street: '',
+  house: '',
+  flat: ''
+};
+
+const ukrPost = {
+  region: '',
+  regionId: '',
+  district: '',
+  districtId: '',
+  city: '',
+  cityId: '',
+  courierOffice: ''
+};
+
+const worldWide = {
+  messenger: '',
+  messengerPhone: '',
+  worldWideCountry: '',
+  stateOrProvince: '',
+  worldWideCity: '',
+  worldWideStreet: '',
+  cityCode: ''
+};
+
+const novaPost = {
+  city: '',
+  courierOffice: ''
+};
+
 export const initialValues = {
   status: '',
   paymentMethod: '',
   isPaid: false,
-  recipient: {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: ''
-  },
+  recipient,
   user_id: '',
   delivery: {
     sentBy: deliveryTypes.selfPickUp,
-    courier: {
-      region: '',
-      district: '',
-      city: '',
-      street: '',
-      house: '',
-      flat: ''
-    },
-    novaPost: {
-      city: '',
-      courierOffice: ''
-    },
-    ukrPost: {
-      region: '',
-      regionId: '',
-      district: '',
-      districtId: '',
-      city: '',
-      cityId: '',
-      courierOffice: ''
-    },
-    worldWide: {
-      messenger: '',
-      messengerPhone: '',
-      worldWideCountry: '',
-      stateOrProvince: '',
-      worldWideCity: '',
-      worldWideStreet: '',
-      cityCode: ''
-    }
+    novaPost,
+    ukrPost,
+    worldWide,
+    courier
   },
   userComment: '',
   items: []
