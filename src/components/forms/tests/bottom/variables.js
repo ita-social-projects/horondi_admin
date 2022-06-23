a26497b7470ea0a8c26577f5bc670c9ff6225dc4
Evhenij Pipa <zhuvchuk1@gmail.com>
2022-06-23 16:40:39 +0300

    bottom

--- src/components/forms/tests/bottom/variables.js
+++ src/components/forms/tests/bottom/variables.js
@@ -1,23 +1,11 @@
 export const mockedInitialValues = {
-  _id: '6043c8acc60c2e4b940189ae',
-  name: [
-    {
-      lang: 'ua',
-      value: 'Низи'
-    },
-    {
-      lang: 'en',
-      value: 'Bottoms'
-    }
-  ],
-  available: true,
-  optionType: 'BOTTOM',
-  images: {
-    large: 'large_byy9rkksa3ukbh_144.jpg',
-    medium: 'medium_byy9rkksa3ukbh_144.jpg',
-    small: 'small_byy9rkksa3ukbh_144.jpg',
-    thumbnail: 'thumbnail_byy9rkksa3ukbh_144.jpg'
-  },
-  absolutePrice: null,
-  relativePrice: 6
+  bottomImage: '',
+  uaName: '',
+  enName: '',
+  color: '',
+  material: '',
+  additionalPriceType: 'ABSOLUTE',
+  additionalPrice: 10,
+  available: false,
+  customizable: false
 };
