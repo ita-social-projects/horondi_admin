fb91f8d25f1dbcf20d4427fea0679b26b74ba7db
dmytruk-y <86732805+dmytruk-y@users.noreply.github.com>
2022-06-29 13:15:55 +0300

    add a badge to the Clients tab (#1403)

--- src/components/nav-menu/nav-menu.styles.js
+++ src/components/nav-menu/nav-menu.styles.js
@@ -50,7 +50,7 @@ export const useStyles = makeStyles((theme) => ({
   sliderTab: {
     paddingLeft: theme.spacing(8)
   },
-  bageRight: {
+  badgeRight: {
     right: theme.spacing(1.5)
   }
 }));
