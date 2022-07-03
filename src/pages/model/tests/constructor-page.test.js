32e3df31c11ed65fcb888fbe3d23af5cb3cc71a8
greentoss <mrsoberman1@gmail.com>
2022-07-03 12:57:44 +0300

    added tests to buttons on constructor-page

--- src/pages/model/tests/constructor-page.test.js
+++ src/pages/model/tests/constructor-page.test.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import ConstructorPage from '../constructor/constructor-page';
 import { store, match } from './constructor-page.variables';
 import { config } from '../../../configs';
 
 const { constructorBasic } = config.labels.model;
+const mockUseHistory = jest.fn();
+const mockDispatch = jest.fn();
+const mockOpenSnackbar = jest.fn();
+const mockEditConstructor = jest.fn();
+const mockDeleteConstructorElement = jest.fn();
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    goBack: mockUseHistory
+  })
+}));
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockEditConstructor
+  // useSelector: jest.fn()
+}));
+
+// jest.mock('../../../utils/use-success-snackbar', () => ({
+//   __esModule: true,
+//   default: () => ({
+//     openSuccessSnackbar: mockOpenSnackbar
+//   })
+// }));
 
 jest.mock('../constructor/constructor-page/constructor-page.styles.js', () => ({
   useStyles: () => ({})
@@ -33,8 +59,38 @@ describe('Testing ConstructorPage', () => {
     );
   });
 
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
   it(`Should render component with tab named as '${constructorBasic}' set as default tab`, () => {
     const firstTab = screen.getAllByText(constructorBasic)[0].closest('button');
     expect(firstTab.getAttribute('aria-selected')).toBe('true');
+  });
+
+  it('should simulate editHandler', async () => {
+    const editBtns = await screen.findAllByTitle(/Редагувати/);
+    fireEvent.click(editBtns[0]);
+    expect(mockEditConstructor).toHaveBeenCalledTimes(4);
+  });
+
+  it('should simulate deleteHandler', async () => {
+    const editBtns = await screen.findAllByTitle(/Видалити/);
+    fireEvent.click(editBtns[0]);
+    expect(mockEditConstructor).toHaveBeenCalledTimes(2);
+  });
+
+  it('is goBack btn has been called', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <ConstructorPage match={match} />
+        </BrowserRouter>
+      </Provider>
+    );
+    const findBackBtn = container.querySelector('[data-cy="back-btn"]');
+    expect(findBackBtn).toBeInTheDocument();
+    fireEvent.click(findBackBtn);
+    expect(mockUseHistory).toHaveBeenCalledTimes(1);
   });
 });
