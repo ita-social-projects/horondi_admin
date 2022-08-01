bac487b30ee31ea5718235e03665626434010195
Yurii Dmytruk <yuri.dmytruk@gmail.com>
2022-08-01 10:31:28 +0300

    Add an error message

--- src/pages/material/material-about-details.js
+++ src/pages/material/material-about-details.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useParams, useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
 import { useQuery } from '@apollo/client';
 import LoadingBar from '../../components/loading-bar';
 import MaterialAboutForm from '../../components/forms/material-about-form';
 import { GET_MATERIALS_BLOCK_BY_ID } from './operations/materials-page.queries';
+import { showErrorSnackbar } from '../../redux/snackbar/snackbar.actions';
+import { config } from '../../configs';
 
 const MaterialAboutDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { ERROR_BOUNDARY_STATUS } = config.errorStatuses;
   const { data, error, loading } = useQuery(GET_MATERIALS_BLOCK_BY_ID, {
     variables: { id },
     fetchPolicy: 'no-cache'
@@ -14,8 +20,14 @@ const MaterialAboutDetails = () => {
 
   const materialsBlock = data?.getMaterialsBlockById;
 
-  if (loading || error) {
+  if (loading) {
     return <LoadingBar />;
+  }
+
+  if (error) {
+    dispatch(showErrorSnackbar(ERROR_BOUNDARY_STATUS));
+    history.push('/');
+    return null;
   }
 
   return (
--- src/pages/material/tests/material-about-details.spec.js
+++ src/pages/material/tests/material-about-details.spec.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/client/testing';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import MaterialAboutDetails from '../material-about-details';
 import { mockMaterialBlock, materialsBlock } from './material-about.variables';
 
+const mockUseParams = jest.fn();
+mockUseParams
+  .mockReturnValueOnce('error13e2f')
+  .mockReturnValueOnce('error13e2f')
+  .mockReturnValue('62013e2fb96ee84338cf5005');
+
+const mockUseHistory = jest.fn();
 jest.mock('react-router', () => ({
-  useParams: () => ({
-    id: '62013e2fb96ee84338cf5005'
-  })
+  useParams: () => ({ id: mockUseParams() }),
+  useHistory: () => ({ push: mockUseHistory })
+}));
+
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useDispatch: () => mockDispatch
 }));
 
 const mockFormComponent = jest.fn();
 mockFormComponent.mockImplementation(({ selectedBlock }) => (
   <div>{selectedBlock.title}</div>
 ));
-jest.mock('../../../components/forms/material-about-form', () => (props) => mockFormComponent(props));
+jest.mock(
+  '../../../components/forms/material-about-form',
+  () => (props) => mockFormComponent(props)
+);
 
 describe('MaterialAboutDetails component tests', () => {
   beforeEach(() => {
@@ -25,6 +39,11 @@ describe('MaterialAboutDetails component tests', () => {
     );
   });
 
+  it('Should handle an error', async () => {
+    await act(async () => new Promise((resolve) => setTimeout(resolve, 0)));
+    expect(mockDispatch).toHaveBeenCalled();
+    expect(mockUseHistory).toHaveBeenCalled();
+  });
   it('Should render the material form', async () => {
     expect(await screen.findByText(materialsBlock.title)).toBeInTheDocument();
   });
