2443d090ae09931009c57cf576ed3414420c202c
Yura Didenko <83120263+Yurenko@users.noreply.github.com>
2022-10-31 11:16:12 +0200

    fixed phone validation when you add order (#1477)

--- src/configs/regexes.js
+++ src/configs/regexes.js
@@ -1,4 +1,6 @@
 export const userNameRegex = /^([a-zа-яіїє]|[',. -][a-zа-яіїє])*$/i;
-export const phoneNumberRegex = /^\d{9}/;
+export const phoneNumberWithoutLettersRegex = /^[0-9 ()+-]+$/;
+export const phoneNumberRegex =
+  /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g;
 export const imgNameRegex = /(thumbnail|small|medium|large)_(.+)/;
 export const onlyNumbersRegex = /^\d*$/;
