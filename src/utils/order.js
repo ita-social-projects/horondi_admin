b3b9ebc9f15644253ef3ff0b85b0be4b0de9a6ca
Yura Didenko <didenkoyura225@gmail.com>
2022-11-02 12:05:22 +0200

    Initial value of payment status is null

--- src/utils/order.js
+++ src/utils/order.js
@@ -139,7 +139,7 @@ export const newOrder = (order) => ({
   paymentStatus: paymentStatusTransfer(order.isPaid, order.paymentStatus)
 });
 
-export const paymentStatusTransfer = (isPaid, paymentStatus) =>
+export const paymentStatusTransfer = (isPaid, paymentStatus = null) =>
   isPaid ? 'PAID' : paymentStatus || 'CREATED';
 
 export const submitStatus = ['CREATED', 'CONFIRMED'];
