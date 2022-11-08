4d4c0ec52567dc18aed2c567bbb571958964c92d
tarasSanotskyi <badsanta2102@gmail.com>
2022-11-08 23:54:11 +0200

    remove test

--- src/pages/promo-code/promo-code-add/tests/promo-code-add.spec.js
+++ src/pages/promo-code/promo-code-add/tests/promo-code-add.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { MockedProvider } from '@apollo/client/testing';
 import { useDispatch } from 'react-redux';
 import { TextField } from '@material-ui/core';
-import { DatePicker } from 'rsuite';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import PromoCodeAdd from '../promo-code-add';
@@ -56,18 +55,6 @@ describe('PromoCodeAdd component tests', () => {
   it('simulate onchange PromoCodeAdd code', async () => {
     const input = wrapper.find(TextField).at(1);
     input.props().onChange({ target: { value: 'secondTextField' } });
-    expect(mockState).toHaveBeenCalled();
-  });
-
-  it('simulate onchange PromoCodeAdd DatePicker', async () => {
-    const dataPicker = wrapper.find(DatePicker).at(0);
-    dataPicker.props().onChange({ value: '14.01.2022' });
-    expect(mockState).toHaveBeenCalled();
-  });
-
-  it('simulate onchange PromoCodeAdd DatePicker', async () => {
-    const dataPicker = wrapper.find(DatePicker).at(1);
-    dataPicker.props().onChange({ value: '29.12.2021' });
     expect(mockState).toHaveBeenCalled();
   });
 });
